d24d2557297b65bf6a5be0153de0fcd06b210124
Elijah Insua <tmpvar@gmail.com>
2010-01-30 19:15:46 -0500

    added node-xml example for a basic view into how this could be used with a parser

---
+++ example/node-xml/run.js
@@ -0,0 +1,46 @@
+var sys = require('sys');
+process.mixin(GLOBAL, require("../../src/level1/core").dom.level1.core);
+
+// git clone git://github.com/robrighter/node-xml.git into ~/.node_libraries
+var xml = require("node-xml/lib/node-xml");
+
+var doc = new Document();
+var currentElement = doc;
+var totalElements = 0;
+var parser = new xml.SaxParser(function(cb) {
+  cb.onStartDocument(function() {
+      
+  });
+  cb.onEndDocument(function() {
+      sys.puts((doc.getElementsByTagName("*").length === totalElements) ? "success" : "fail");
+  });
+  cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
+      totalElements++;
+      var element = doc.createElement(elem);
+      currentElement.appendChild(element);
+      currentElement = element;
+      sys.puts("=> Started: " + elem + " uri="+uri +" (Attributes: " + JSON.stringify(attrs) + " )");
+  });
+  cb.onEndElementNS(function(elem, prefix, uri) {
+      currentElement = currentElement.parentNode;
+      sys.puts("<= End: " + elem + " uri="+uri + "\n");
+  });
+  cb.onCharacters(function(chars) {
+     
+  });
+  cb.onCdata(function(cdata) {
+      sys.puts('<CDATA>'+cdata+"</CDATA>");
+  });
+  cb.onComment(function(msg) {
+      sys.puts('<COMMENT>'+msg+"</COMMENT>");
+  });
+  cb.onWarning(function(msg) {
+      sys.puts('<WARNING>'+msg+"</WARNING>");
+  });
+  cb.onError(function(msg) {
+      sys.puts('<ERROR>'+JSON.stringify(msg)+"</ERROR>");
+  });
+});
+
+//example read from file
+parser.parseFile("example.xml");
