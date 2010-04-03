fd192e052099abc8d0af00a40691e0b76956c865
Elijah Insua <tmpvar@gmail.com>
2010-04-03 16:03:26 -0400

    changed GLOBAL to global, forced inclusion of local mjsunit (modified method sigs)

--- test/runner.js
+++ test/runner.js
@@ -1,25 +1,25 @@
 var sys = require("sys");
-process.mixin(GLOBAL, require("../lib/level1/core").dom.level1.core);
-process.mixin(GLOBAL, require("./mjsunit"));
-process.mixin(GLOBAL, require("./DOMTestCase"));
+process.mixin(global, require("../lib/level1/core").dom.level1.core);
+process.mixin(global, require(__dirname + "/mjsunit"));
+process.mixin(global, require("./DOMTestCase"));
 
 // Compat Layer
-GLOBAL.builder = { 
+global.builder = { 
   contentType: "",
   type: "",
   testDirectory: ""
 };
 
-GLOBAL.load = function(docRef, doc, name) {
-  return require("./" + GLOBAL.builder.testDirectory + "/files/" + name + "." + GLOBAL.builder.type)[name]();
+global.load = function(docRef, doc, name) {
+  return require("./" + global.builder.testDirectory + "/files/" + name + "." + global.builder.type)[name]();
 };
 
-GLOBAL.checkInitialization = function() {
+global.checkInitialization = function() {
   return null;
 };
 
 
-GLOBAL.debug = function(val) {
+global.debug = function(val) {
   var str;
   try {
     str = JSON.stringify(val, null, "  ");
@@ -34,51 +34,51 @@ GLOBAL.debug = function(val) {
 
 var suites = {
   "level1/core" : { cases: require("./level1/core").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "text/xml";
-      GLOBAL.builder.type          = "xml";
-      GLOBAL.builder.testDirectory = "level1/core";  
+      global.builder.contentType   = "text/xml";
+      global.builder.type          = "xml";
+      global.builder.testDirectory = "level1/core";  
     }
   },
   "level1/html" : { cases: require("./level1/html").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "text/html";
-      GLOBAL.builder.type          = "html";
-      GLOBAL.builder.testDirectory = "level1/html";  
+      global.builder.contentType   = "text/html";
+      global.builder.type          = "html";
+      global.builder.testDirectory = "level1/html";  
     }
   },
   "level1/svg"  : { cases: require("./level1/svg").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "image/svg+xml";
-      GLOBAL.builder.type          = "svg";
-      GLOBAL.builder.testDirectory = "level1/svg";  
+      global.builder.contentType   = "image/svg+xml";
+      global.builder.type          = "svg";
+      global.builder.testDirectory = "level1/svg";  
     }
   },
   "browser"     : { cases: require("./browser").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "text/html";
-      GLOBAL.builder.type          = "html";
-      GLOBAL.builder.testDirectory = "browser";  
+      global.builder.contentType   = "text/html";
+      global.builder.type          = "html";
+      global.builder.testDirectory = "browser";  
     }
   },
   "level2/core" : { cases: require("./level2/core").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "text/xml";
-      GLOBAL.builder.type          = "xml";
-      GLOBAL.builder.testDirectory = "level2/core"; 
+      global.builder.contentType   = "text/xml";
+      global.builder.type          = "xml";
+      global.builder.testDirectory = "level2/core"; 
     }
   },
   "level2/html" : { cases: require("./level2/html").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "text/html";
-      GLOBAL.builder.type          = "html";
-      GLOBAL.builder.testDirectory = "level2/html";  
+      global.builder.contentType   = "text/html";
+      global.builder.type          = "html";
+      global.builder.testDirectory = "level2/html";  
     }
   },
  "level3/core" : { cases: require("./level3/core").tests, setUp : function() {
-     GLOBAL.builder.contentType   = "text/xml";
-     GLOBAL.builder.type          = "xml";
-     GLOBAL.builder.testDirectory = "level3/core";  
+     global.builder.contentType   = "text/xml";
+     global.builder.type          = "xml";
+     global.builder.testDirectory = "level3/core";  
    }
  },
  "level3/ls"   : { cases: require("./level3/ls").tests, setUp : function() {
-      GLOBAL.builder.contentType   = "text/html";
-      GLOBAL.builder.type          = "html";
-      GLOBAL.builder.testDirectory = "level3/ls";  
+      global.builder.contentType   = "text/html";
+      global.builder.type          = "html";
+      global.builder.testDirectory = "level3/ls";  
     }
   }
 };
