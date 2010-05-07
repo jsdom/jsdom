a71aa92d8dfca59104fc82c43d1c3afd77e89303
Elijah Insua <tmpvar@gmail.com>
2010-05-06 22:50:19 -0400

    jquery fail

--- example/jquery/run.js
+++ example/jquery/run.js
@@ -1,26 +1,29 @@
 var sys = require("sys"), 
     dom = require("../../lib/level1/core").dom.level1.core,
     fs = require("fs");
-window = global;
+    
+var dom = require("../../lib/browser").windowAugmentation(dom);
+
+var window = global;
+global.window = global;
 global.document =  new dom.Document();
 global.navigator = { userAgent: "node-js" };
 
 fs.readFile(__dirname + "/jquery.js", function(err, data) {
-
+  var html = window.document.createElement("html");
+  window.document.appendChild(html);
   for (var i = 0; i<10; i++)
   {
     var p = window.document.createElement("p")
     p.setAttribute("class", "paragraph-" + i);
     p.appendChild(window.document.createTextNode("Item #" + i));
   }
-
+sys.puts(global.document.documentElement);
+  global.document.compareDocumentPosition = function() {};
+  
+  eval.call(global, data);
+  sys.puts(JSON.stringify(data));
   sys.puts(jQuery("p#paragraph-5", window.document).length);
-  
-  global.document.documentElement.compareDocumentPosition = function() {};
-  
-  sys.puts(sys.inspect(eval(data)));
-
-  
 
   //var jQuery = require("./node-jquery").jQueryInit(window, navigator);
 
