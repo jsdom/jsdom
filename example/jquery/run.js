6169ea8ae86e40aa7249af44e0aeab3941176333
Elijah Insua <tmpvar@gmail.com>
2010-05-07 17:58:27 -0400

    jQuery example is functional, had to stub out events / document.compareDocumentPosition, but selectors / text / etc works

--- example/jquery/run.js
+++ example/jquery/run.js
@@ -1,34 +1,27 @@
 var sys = require("sys"), 
-    dom = require("../../lib/level1/core").dom.level1.core,
+    dom = require(__dirname +"/../../lib/level1/core").dom.level1.core,
     fs = require("fs");
     
-global.window = require("../../lib/browser").windowAugmentation(dom);
-global.document = global.window.document;
-global.location = global.window.location;
+var window = global.window = require(__dirname + "/../../lib/browser").windowAugmentation(dom);
+var document = global.document = global.window.document;
+var location = global.location = global.window.location;
 var window = global;
-global.navigator = { userAgent: "node-js" };
+var navigator = global.navigator = { userAgent: "node-js" };
 
 fs.readFile(__dirname + "/jquery.js", function(err, data) {
-  for (var i = 0; i<10; i++)
-  {
-    var p = window.document.createElement("p")
-    p.setAttribute("class", "p" + i);
-    document.body.appendChild(window.document.createTextNode("Item #" + i));
-  sys.puts(document.body.children.length);
-  }
-  
-  
-  global.window.document.compareDocumentPosition = function() {};
-    dom.Node.prototype.addEventListener = 
-    global.window.document.addEventListener = function() {};
 
-  eval(data);
-  
-  
-  
-  jQuery("p0").each(function() {
-    sys.puts(sys.inspect(this.tagName));
-  });
-  
-  sys.puts(jQuery("").length);
+  global.window.document.compareDocumentPosition = function() {};
+  dom.Node.prototype.addEventListener = window.addEventListener = window.document.addEventListener = function() {};
+
+  try {
+    eval(data);
+  } catch (e) {
+    sys.puts(sys.inspect(e.stack, true));
+  }
+
+  // Doing this requires you setup a parser, easiest way is to put node-htmlparser.js into 
+  // ~/.node_libraries/
+  jQuery(document.body).append("<div class='testing'>Hello World, It works!</div>");
+  sys.puts(jQuery(".testing").text());
+ 
 })
