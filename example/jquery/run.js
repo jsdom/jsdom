63c14566fbb578567fb24e9f31c4667955eb7d3e
Elijah Insua <tmpvar@gmail.com>
2010-05-12 19:20:39 -0400

    fixed jquery example

--- example/jquery/run.js
+++ example/jquery/run.js
@@ -1,27 +1,27 @@
 var sys = require("sys"), 
     dom = require(__dirname +"/../../lib/level1/core").dom.level1.core,
     fs = require("fs");
+
     
-var window = global.window = require(__dirname + "/../../lib/browser").windowAugmentation(dom);
-var document = global.document = global.window.document;
-var location = global.location = global.window.location;
-var window = global;
-var navigator = global.navigator = { userAgent: "node-js" };
+var window = require(__dirname + "/../../lib/browser").windowAugmentation(dom);
+var document = window.document = window.document;
+var location = window.location = window.location;
+var navigator = window.navigator = { userAgent: "node-js" };
+global.window = window;
 
 fs.readFile(__dirname + "/jquery.js", function(err, data) {
-
   global.window.document.compareDocumentPosition = function() {};
   dom.Node.prototype.addEventListener = window.addEventListener = window.document.addEventListener = function() {};
 
   try {
-    eval(data);
+    eval(data.toString());
   } catch (e) {
     sys.puts(sys.inspect(e.stack, true));
   }
 
   // Doing this requires you setup a parser, easiest way is to put node-htmlparser.js into 
   // ~/.node_libraries/
-  jQuery(document.body).append("<div class='testing'>Hello World, It works!</div>");
-  sys.puts(jQuery(".testing").text());
- 
-})
+
+  window.jQuery(document.body).append("<div class='testing'>Hello World, It works!</div>");
+  sys.puts(window.jQuery(".testing").text());
+});
