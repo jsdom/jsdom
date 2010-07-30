36f1cbd3bfcf07291b628b8ec414ade1cf66f60e
Elijah Insua <tmpvar@gmail.com>
2010-07-29 23:21:52 -0400

    made jquery example work with the new simplified api, had to fix up a couple things to make it consistent.

--- example/jquery/run.js
+++ example/jquery/run.js
@@ -1,17 +1,17 @@
-var sys = require("sys"), fs = require("fs");
-
-var dom = require("../../lib/jsdom/level1/core").dom.level1.core;
-var window = require("../../lib/jsdom/browser").windowAugmentation(dom).window;
-var Script = process.binding('evals').Script;
+var sys    = require("sys"),
+    fs     = require("fs"),
+    jsdom  = require(__dirname + "/../../lib/jsdom").jsdom,
+    window = jsdom().makeWindow(),
+    Script = process.binding('evals').Script;
 
 fs.readFile(__dirname + "/jquery.js", function(err, data) {
-  
   try {
-    Script.runInNewContext(data.toString(), {window: window, location: window.location, navigator: window.navigator});
+    Script.runInNewContext(data.toString(), window);
   } catch(e){
     sys.puts(sys.inspect(e));
+    process.exit();
   }
+
   window.jQuery('body').append("<div class='testing'>Hello World, It works!</div>");
   sys.puts(window.jQuery(".testing").text());
-  
 });
