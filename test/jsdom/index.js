c4b8296fcfd356b7ac6b02ea79ef096f5cc2bcef
Elijah Insua <tmpvar@gmail.com>
2010-09-10 20:46:58 -0400

    removed level3 test html's, added stupid simple jQuery compareDocumentPosition test

--- test/jsdom/index.js
+++ test/jsdom/index.js
@@ -5,15 +5,21 @@ exports.tests = {
     var window = jsdom.jsdom().createWindow();
     assertNotNull("window must be a new object!", window);
   },
-  
+
   jquerify : function() {
     jsdom.jQueryify(jsdom.jsdom().createWindow(), 
                     __dirname + "/../../example/jquery/jquery.js", 
                     function(window, jQuery) 
     {
-        assertNotNull("jQuery should be attached to the window", window.jQuery.find);
-        assertNotNull("jQuery should be attached to the window", jQuery.find);
+      assertNotNull("jQuery should be attached to the window", window.jQuery.find);
+      assertNotNull("jQuery should be attached to the window", jQuery.find);
+      var caught = false;
+      try {
+        jQuery("#a .test", window.body);
+      } catch (e) {
+        caught = true;
+      }
+      assertFalse("compareDocumentPosition should not fail", caught);
     });
   }
-  
 };
