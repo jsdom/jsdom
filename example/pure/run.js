ee0cf84a143d677ea56c3cec698967f494c4d62f
Felix Gnass <felix.gnass@neteye.de>
2010-11-11 13:15:17 +0100

    Example requires level2/html

--- example/pure/run.js
+++ example/pure/run.js
@@ -1,5 +1,5 @@
 var browser = require("../../lib/jsdom/browser/index");
-var dom = new browser.browserAugmentation(require("../../lib/jsdom/level2/core").dom.level2.core);
+var dom = new browser.browserAugmentation(require("../../lib/jsdom/level2/html").dom.level2.html);
 var sax = require("./sax");
 var sys = require("sys");
 
