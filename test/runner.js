833cac71fa3c4d6c4eeea2f764d17dc792e3eb39
Daniel Cassidy <mail@danielcassidy.me.uk>
2011-02-13 12:46:06 +0000

    More conformance fixes to createDocument: * Accept null namespaceURI and qualifiedName: this creates a document containing no document element. * Set doctype.ownerDocument. * Add the doctype as a child node of the document.
    Also add new test cases to catch regressions on these fixes. I've added the new test cases to a new suite, level2/core-extra.js, to indicate that they test DOM Level 2 Core, but aren't part of the standard W3C test suite.

--- test/runner.js
+++ test/runner.js
@@ -95,6 +95,13 @@ var suites = {
       global.builder.testDirectory = "level2/core";
     }
   },
+  "level2/core-extra" : { cases: require("./level2/core-extra").tests, setUp : function () {
+      mixin(global, require("../lib/jsdom/level2/core").dom.level2.core);
+      global.builder.contentType   = "text/xml";
+      global.builder.type          = "xml";
+      global.builder.testDirectory = "level2/core";
+    }
+  },
   "level2/events" : { cases: require("./level2/events").tests, setUp : function() {
       mixin(global, require("../lib/jsdom/level2/events").dom.level2.events);
       global.events = require("../lib/jsdom/level2/events").dom.level2.events;
