16fde783ad902734ad65bcb0c4f3dd45d978d8db
waslogic <waslogic@gmail.com>
2011-03-01 18:01:34 -0500

    for some reason, I wrote a TestSuite Object... I should look into nodeunit instead

--- test/level2/extra_test.js
+++ test/level2/extra_test.js
@@ -1,50 +1,105 @@
 /* Contains extra test cases for parts of DOM Level 2 that aren't covered by
  * the standard W3C test cases. */
-// var assert = require('assert'),
-    // core = require("../../lib/jsdom/level2/core").dom.level2.core;
 
-var assert, core;
+global.assert = require('assert');
 
-exports.hooks = {
-  before_all: function() {
-    assert = require('assert');
-    core = require("../../lib/jsdom/level2/core").dom.level2.core;
-  },
-  before_each: function() {
-  },
-  after_each: function() {},
-  after_all: function() {
-    assert = undefined;
-    core = undefined;
+function TestSuite(name) {
+  this.name = name;
+  this.tests = {};
+
+  TestSuite.prototype.before_all = function(){}
+  TestSuite.prototype.before_each = function(){}
+  TestSuite.prototype.after_each = function(){}
+  TestSuite.prototype.after_all = function(){}
+
+  TestSuite.prototype.run_test = function (test) {
+    var result = {name: test, result: 'PASSED'}
+    this.before_each.apply(this.tests);
+    try {
+      this.tests[test]();
+    } catch(e) {
+      result.result = 'FAILED';
+      result.error = e;
+    }
+    this.after_each.apply(this.tests);
+    return(result);
+  }
+
+  TestSuite.prototype.run = function () {
+    // TODO: eventually, accept a single test, a list of tests, an array of tests, or nothing
+    var valid_tests = this.test_names(),
+        results = [],
+        self = this;
+    var tests = get_values(arguments) || valid_tests;
+    this.before_all.apply(this.tests);
+    tests.forEach(function(test){
+      if (valid_tests.indexOf(test) > -1) {
+        results.push(self.run_test(test));
+      } else {
+        results.push({name: test, result: 'NOT FOUND'})
+      }
+    });
+    this.after_all.apply(this.tests);
+    return(results);
+  }
+
+  TestSuite.prototype.test_names = function () {
+    var names = [];
+    for (var test in this.tests) {
+      if (this.tests.hasOwnProperty(test)) {
+        names.push(test);
+      }
+    }
+    return(names);
+  }
+
+  // private
+  var get_values = function(args) {
+    if (args.length == 0) return;
+    var values = [];
+    for (var i=0; i < args.length; i++) {
+      values.push(args[i]);
+    }
+    return(values);
   }
 }
 
-exports.tests = {
+var suite = new TestSuite('level2/extra');
+suite.before_all = function() {
+  this.core = require("../../lib/jsdom/level2/core").dom.level2.core;
+}
+suite.before_each = function() {
+  this.dom = new this.core.DOMImplementation();
+}
+suite.after_each = function() {
+  this.dom = undefined;
+  delete(this.dom);
+}
+suite.after_all = function() {
+  this.core = undefined;
+  delete(this.core);
+}
+
+suite.tests = {
   create_empty_document: function () {
-    var dom = new core.DOMImplementation();
-    var document = dom.createDocument(null, null, null);
+    var document = this.dom.createDocument(null, null, null);
     assert.equal(0, document.childNodes.length, "Document shouldn't contain any nodes");
   },
   create_document_with_namespaceURI_but_not_qualifiedName: function () {
-    var dom = new core.DOMImplementation();
-    assert.throws(function () { dom.createDocument("http://example.org/motorcycle", null, null); },
-                  core.DOMException,'createDocument accepted invalid arguments');
-  },
-  create_document_with_namespaceURI_but_not_qualifiedName_TEST: function () {
-    var dom = new core.DOMImplementation();
-    assert.throws(function () { dom.createDocument("http://example.org/motorcycle", null, null); },
-                  core.DOMException.NAMESPACE_ERR, 'createDocument accepted invalid arguments');
+    assert.throws(function () { this.dom.createDocument("http://example.org/motorcycle", null, null); },
+                  this.core.DOMException.NAMESPACE_ERR, 'createDocument accepted invalid arguments');
   },
   doctype_ownerDocument: function () {
-    var dom = new core.DOMImplementation();
-    var doctype = dom.createDocumentType("bananas");
-    var document = dom.createDocument(null, null, doctype);
+    var doctype = this.dom.createDocumentType("bananas");
+    var document = this.dom.createDocument(null, null, doctype);
     assert.equal(document, doctype.ownerDocument, 'Doctype does not belong to the document');
   },
   doctype_child_of_ownerDocument: function () {
-    var dom = new core.DOMImplementation();
-    var doctype = dom.createDocumentType("hatstand");
-    var document = dom.createDocument(null, null, doctype);
+    var doctype = this.dom.createDocumentType("hatstand");
+    var document = this.dom.createDocument(null, null, doctype);
     assert.equal(doctype, document.firstChild, 'Doctype is not a child of the document');
   }
 }
+
+exports.run = function() { return(suite.run.apply(suite, arguments)); }
+exports.test_names = suite.test_names();
