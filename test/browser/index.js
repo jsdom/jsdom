36cf2c9faaaf118417cd9233a5ff0a19ae1a7e30
waslogic <waslogic@gmail.com>
2011-03-11 17:20:50 -0500

    browswer testsuite now working with nodeunit

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,218 +1,190 @@
+var dom = require("../../lib/jsdom/level2/core").dom.level2.core;
+var browser = require("../../lib/jsdom/browser/index").browserAugmentation(dom);
+
 exports.tests = {
-
-  notfound_getelementsbyclassname : function() {
-
-      var doc = new browser.Document();
-
-      var html = doc.createElement("html");
-      doc.appendChild(html);
-
-      var body = doc.createElement("body");
-      html.appendChild(body);
-
-      var p = doc.createElement("p");
-      p.className = "unknown";
-      body.appendChild(p);
-
-      var elements = doc.getElementsByClassName("first-p");
-      assertEquals("no results", 0, elements.length);
-  },
-
-
-  basic_getelementsbyclassname : function() {
-
-      var doc = new browser.Document();
-
-      var html = doc.createElement("html");
-      doc.appendChild(html);
-
-      var body = doc.createElement("body");
-      html.appendChild(body);
-
-      var p = doc.createElement("p");
-      p.className = "first-p";
-      body.appendChild(p);
-
-      var elements = doc.getElementsByClassName("first-p");
-      assertSame("p and first-p", p, elements.item(0));
-  },
-
-  multiple_getelementsbyclassname : function() {
-
-      var doc = new browser.Document();
-
-      var html = doc.createElement("html");
-      doc.appendChild(html);
-
-      var body = doc.createElement("body");
-      html.appendChild(body);
-
-      var p = doc.createElement("p");
-      p.className = "first-p second third";
-      body.appendChild(p);
-
-      var first = doc.getElementsByClassName("first-p");
-      assertSame("p and first-p", p, first.item(0));
-
-      var second = doc.getElementsByClassName("second");
-      assertSame("p and second", p, second.item(0));
-
-      var third = doc.getElementsByClassName("third");
-      assertSame("p and third", p, third.item(0));
-  },
-
-  testclassnameworksasexpected : function() {
-      var doc = new browser.Document();
-      var p = doc.createElement("p");
-      p.setAttribute("class", "first-p");
-      assertSame("class attribute is same as className", p.className,"first-p");
-
-      p.className += " second";
-      assertSame("className getter/setter", p.className,"first-p second");
-  },
-
-  basic_getelementbyid : function() {
-
-      var doc = new browser.Document();
-
-      var html = doc.createElement("html");
-      doc.appendChild(html);
-
-      var body = doc.createElement("body");
-      html.appendChild(body);
-
-      var p = doc.createElement("p");
-      p.id = "theid";
-      body.appendChild(p);
-
-      var element = doc.getElementById("theid");
-      assertSame("p and #theid", p, element);
-  },
-  nonexistant_getelementbyid : function() {
-
-      var doc = new browser.Document();
-
-      var html = doc.createElement("html");
-      doc.appendChild(html);
-
-      var body = doc.createElement("body");
-      html.appendChild(body);
-
-      var p = doc.createElement("p");
-      p.id = "theid";
-      body.appendChild(p);
-
-      var element = doc.getElementById("non-existant-id");
-      assertSame("p and #theid", null, element);
-  },
-  remove_nonexistantattribute : function() {
-      var doc = new browser.Document();
-
-      var html = doc.createElement("html");
-      doc.appendChild(html);
-
-      var body = doc.createElement("body");
-      html.appendChild(body);
-
-      exception = false;
-      try {
-        removedNode = body.removeAttribute("non-existant");
-      }
-      catch(ex) {
-        exception = true;
-      }
-      assertFalse("setValue_throws_NO_MODIFICATION_ERR", exception);
-  },
-  render_singletag : function() {
-      var doc = new browser.Document();
-
-      var p = doc.createElement("p");
-
-      var img = doc.createElement("img");
-      p.appendChild(img);
-      var out = p.outerHTML;
-      assertNull("end tag not included in output", out.match(/<\/img>/));
-  },
-  parse_scripttags : function() {
+  notfound_getelementsbyclassname: function(test) {
     var doc = new browser.Document();
+    var html = doc.createElement("html");
+    doc.appendChild(html);
+    var body = doc.createElement("body");
+    html.appendChild(body);
+    var p = doc.createElement("p");
+    p.className = "unknown";
+    body.appendChild(p);
+    var elements = doc.getElementsByClassName("first-p");
+    test.equal(elements.length, 0, "no results");
+    test.done();
+  },
 
+  basic_getelementsbyclassname: function(test) {
+    var doc = new browser.Document();
+    var html = doc.createElement("html");
+    doc.appendChild(html);
+    var body = doc.createElement("body");
+    html.appendChild(body);
+    var p = doc.createElement("p");
+    p.className = "first-p";
+    body.appendChild(p);
+    var elements = doc.getElementsByClassName("first-p");
+    test.equal(elements.item(0), p, 'p and first-p');
+    test.done();
+  },
+
+  multiple_getelementsbyclassname: function(test) {
+    var doc = new browser.Document();
+    var html = doc.createElement("html");
+    doc.appendChild(html);
+    var body = doc.createElement("body");
+    html.appendChild(body);
+    var p = doc.createElement("p");
+    p.className = "first-p second third";
+    body.appendChild(p);
+    var first = doc.getElementsByClassName("first-p").item(0);
+    var second = doc.getElementsByClassName("second").item(0);
+    var third = doc.getElementsByClassName("third").item(0);
+    test.equal(first, p, 'p and first-p');
+    test.equal(second, p, 'p and second');
+    test.equal(third, p, 'p and third');
+    test.done();
+  },
+
+  testclassnameworksasexpected: function(test) {
+    var doc = new browser.Document();
+    var p = doc.createElement("p");
+    p.setAttribute("class", "first-p");
+    test.equal(p.className, 'first-p', 'class attribute is same as className');
+    p.className += " second";
+    test.equal(p.className, 'first-p second', 'className getter/setter');
+    test.done();
+  },
+
+  basic_getelementbyid: function(test) {
+    var doc = new browser.Document();
+    var html = doc.createElement("html");
+    doc.appendChild(html);
+    var body = doc.createElement("body");
+    html.appendChild(body);
+    var p = doc.createElement("p");
+    p.id = "theid";
+    body.appendChild(p);
+    var element = doc.getElementById("theid");
+    test.equal(element, p, "p and #theid");
+    test.done();
+  },
+
+  nonexistant_getelementbyid: function(test) {
+    var doc = new browser.Document();
+    var html = doc.createElement("html");
+    doc.appendChild(html);
+    var body = doc.createElement("body");
+    html.appendChild(body);
+    var p = doc.createElement("p");
+    p.id = "theid";
+    body.appendChild(p);
+    var element = doc.getElementById("non-existant-id");
+    test.equal(element, null, "p and #theid");
+    test.done();
+  },
+
+  remove_nonexistantattribute: function(test) {
+    var doc = new browser.Document();
+    var html = doc.createElement("html");
+    doc.appendChild(html);
+    var body = doc.createElement("body");
+    html.appendChild(body);
+    test.doesNotThrow(function(){ body.removeAttribute("non-existant"); }), 'setValue_throws_NO_MODIFICATION_ERR';
+    test.done();
+  },
+
+  render_singletag: function(test) {
+    var doc = new browser.Document();
+    var p = doc.createElement("p");
+    var img = doc.createElement("img");
+    p.appendChild(img);
+    var out = p.outerHTML;
+    test.equal(out.match(/<\/img>/), null, 'end tag not included in output')
+    test.done();
+  },
+
+  parse_scripttags: function(test) {
+    var doc = new browser.Document();
     var head = doc.createElement("head");
     var scriptHtml = '<script>alert("hello world")</script>';
     head.innerHTML = scriptHtml;
-
-    assertEquals("original and processed", head.innerHTML, scriptHtml);
-
+    test.equal(scriptHtml, head.innerHTML, "original and processed");
+    test.done();
   },
-  parse_styletags : function() {
-    var doc = new browser.Document();
 
+  parse_styletags: function(test) {
+    var doc = new browser.Document();
     var head = doc.createElement("head");
     var styleHtml = '<style>body: {color: #fff;}</style>';
     head.innerHTML = styleHtml;
-
-    assertEquals("original and processed", head.innerHTML, styleHtml);
-
+    test.equal(styleHtml, head.innerHTML, "original and processed");
+    test.done();
   },
-  parse_doublespacetags : function() {
-    var doc = new browser.Document();
 
+  parse_doublespacetags: function(test) {
+    var doc = new browser.Document();
     var html = '<html><body  class="testing" /></html>';
-
-    exception = false;
-    try {
-      doc.innerHTML = html;
-    }
-    catch(ex) {
-      exception = true;
-    }
-    assertFalse("setValue_throws_INVALID_CHARACTER_ERR", exception);
+    test.doesNotThrow(function(){ doc.innerHTML = html; }), 'setValue_throws_INVALID_CHARACTER_ERR';
+    test.done();
   },
-  serialize_styleattribute : function() {
+
+  serialize_styleattribute: function(test) {
     var doc = new browser.Document();
+		var domToHtml = require('../../lib/jsdom/browser/domtohtml');
     doc.appendChild(doc.createElement('html'));
     doc.documentElement.style.color = 'black';
     doc.documentElement.style.backgroundColor = 'white';
-    assertEquals('',
-              '<html style="color: black; background-color: white"></html>\r\n',
-              require('../../lib/jsdom/browser/domtohtml').domToHtml(doc));
+    test.equal(domToHtml.domToHtml(doc), '<html style="color: black; background-color: white"></html>\r\n', '');
+    test.done();
   },
-  innerhtml_removeallchildren: function() {
+
+  innerhtml_removeallchildren: function(test) {
     var doc = new browser.HTMLDocument();
     doc.write('<html><body><p></p><p></p></body></html>');
     doc.body.innerHTML = '';
-    assertTrue('still has children', doc.body.childNodes.length == 0);
+    test.equal(doc.body.childNodes.length, 0, 'still has children');
+    test.done();
   },
-  serialize_html5_doctype : function () {
+
+  serialize_html5_doctype: function(test) {
     var dom = new browser.DOMImplementation();
     var doctype = dom.createDocumentType('html');
     var document = dom.createDocument(null, null, doctype);
-    assertTrue('HTML 5 doctype did not serialize correctly',
-        /^\s*<!DOCTYPE html>/.test(document.outerHTML));
+    var regexp = /^\s*<!DOCTYPE html>/;
+    test.ok(regexp.test(document.outerHTML), 'HTML 5 doctype did not serialize correctly');
+    test.done();
   },
-  serialize_html4_strict_doctype : function () {
+
+  serialize_html4_strict_doctype: function(test) {
     var dom = new browser.DOMImplementation();
-    var doctype = dom.createDocumentType('html',
-        '-//W3C//DTD HTML 4.01//EN',
-        'http://www.w3.org/TR/html4/strict.dtd');
+    var doctype = dom.createDocumentType('html', '-//W3C//DTD HTML 4.01//EN', 'http://www.w3.org/TR/html4/strict.dtd');
     var document = dom.createDocument(null, null, doctype);
-    assertTrue('HTML 4 strict doctype did not serialize correctly',
-        /^\s*<!DOCTYPE html PUBLIC "-\/\/W3C\/\/DTD HTML 4.01\/\/EN" "http:\/\/www.w3.org\/TR\/html4\/strict.dtd">/.
-            test(document.outerHTML));
+    var regexp = /^\s*<!DOCTYPE html PUBLIC "-\/\/W3C\/\/DTD HTML 4.01\/\/EN" "http:\/\/www.w3.org\/TR\/html4\/strict.dtd">/;
+    test.ok(regexp.test(document.outerHTML), 'HTML 4 strict doctype did not serialize correctly');
+    test.done();
   },
-  serialize_system_doctype : function () {
+
+  serialize_system_doctype: function(test) {
     var dom = new browser.DOMImplementation();
     var doctype = dom.createDocumentType('foo', null, 'foo.dtd');
     var document = dom.createDocument(null, null, doctype);
-    assertTrue('Doctype did not serialize correctly',
-        /^\s*<!DOCTYPE foo SYSTEM "foo.dtd">/.test(document.outerHTML));
+    var regexp = /^\s*<!DOCTYPE foo SYSTEM "foo.dtd">/;
+    test.ok(regexp.test(document.outerHTML), 'Doctype did not serialize correctly');
+    test.done();
   },
-  serialize_doctype_containing_quotes : function () {
+
+  serialize_doctype_containing_quotes: function(test) {
     var dom = new browser.DOMImplementation();
     var doctype = dom.createDocumentType('foo', null, 'foo "bar".dtd');
     var document = dom.createDocument(null, null, doctype);
-    assertTrue('Doctype did not serialize correctly',
-        /^\s*<!DOCTYPE foo SYSTEM \'foo "bar".dtd\'>/.test(document.outerHTML));
+    var regexp = /^\s*<!DOCTYPE foo SYSTEM \'foo "bar".dtd\'>/;
+    test.ok(regexp.test(document.outerHTML), 'Doctype did not serialize correctly');
+    test.done();
   },
+
   parse_doctype_containing_newline : function() {
     var html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\n \
              "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n<html></html>',
@@ -220,6 +192,7 @@ exports.tests = {
     doc.innerHTML = html;
     assertTrue('doctype should not be falsy', !!doc.doctype);
   },
+
   basic_nodelist_indexOf : function() {
     var doc = new browser.Document();
 
@@ -241,6 +214,7 @@ exports.tests = {
     var index = body.childNodes.indexOf(span);
     assertEquals("indexOf 'span' in childNodes", 2, index);
   },
+
   nonexistant_nodelist_indexOf : function() {
     var doc = new browser.Document();
 
