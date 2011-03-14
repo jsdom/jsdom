febebaecd79d8aeb72d44189680d99efff15658d
waslogic <waslogic@gmail.com>
2011-03-14 10:53:41 -0400

    window tests converted to nodeunit

--- test/window/index.js
+++ test/window/index.js
@@ -1,21 +1,21 @@
+var dom = require("../../lib/jsdom/level1/core").dom.level1.core;
+var window = require("../../lib/jsdom/browser/index").windowAugmentation(dom);
+
 exports.tests = {
- addmetatohead : function() {
+  addmetatohead: function(test) {
     var meta = window.document.createElement("meta");
     window.document.getElementsByTagName("head").item(0).appendChild(meta);
     var elements = window.document.getElementsByTagName("head").item(0).childNodes;
-
-    assertTrue("last element should be the new meta tag",
-                elements.item(elements.length-1) === meta); 
-    assertTrue("meta should not be stringified with a closing tag",
-               window.document.innerHTML.indexOf("<meta>") > -1 &&
-               window.document.innerHTML.indexOf("</meta>") === -1
-               );
+    test.strictEqual(elements.item(elements.length-1), meta, 'last element should be the new meta tag');
+    test.ok(window.document.innerHTML.indexOf("<meta>") > -1, 'meta should have open tag');
+    test.strictEqual(window.document.innerHTML.indexOf("</meta>"), -1, 'meta should not be stringified with a closing tag');
+    test.done();
   },
-  ensure_a_default_window_has_a_window_location_href : function() {
-    var rurl     = /^([\w\+\.\-]+:)\/\/([^\/?#:]*)(?::(\d+))?/,
-        urlParts = rurl.exec(window.location.href);
-console.log(window.location.href, urlParts)
-    assertTrue("url shouldnt be blank", urlParts.length > 1);
 
+  ensure_a_default_window_has_a_window_location_href: function(test) {
+    var rurl = /^([\w\+\.\-]+:)\/\/([^\/?#:]*)(?::(\d+))?/,
+        urlParts = rurl.exec(window.location.href);
+    test.ok(urlParts.length > 1, 'url shouldnt be blank');
+    test.done();
   }
 };
