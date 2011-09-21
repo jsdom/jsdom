7783899b6215d261e054bafaf9c1b78ec7778684
Derek Lindahl <dlindahl@customink.com>
2011-09-21 08:29:14 -0400

    Added configuration of `document.cookie` via `jsdom.env`

--- test/jsdom/index.js
+++ test/jsdom/index.js
@@ -237,8 +237,28 @@ exports.tests = {
       document : { referrer:'https://github.com/tmpvar/jsdom' },
       done: function(errors, window) {
         test.equal(errors, null, 'errors should be null');
-        test.notEqual(window.document._referrer, null, 'window.document.referrer should not be null');
+        test.notEqual(window.document._referrer, null, 'window.document._referrer should not be null');
         test.equal(window.document._referrer, 'https://github.com/tmpvar/jsdom', 'window.document._referrer should match the configured value');
+        test.done();
+      }
+    })
+  },
+
+  env_with_document_cookie : function(test) {
+    var cookie,
+        future = new Date(),
+        html = "<html><body><p>hello world!</p></body></html>";
+
+    future.setTime( future.getTime() + (24 * 60 * 60 * 1000) )
+    cookie = 'key=value; expires='+future.toGMTString()+'; path=/';
+
+    jsdom.env({
+      html : html,
+      document : { cookie:cookie },
+      done: function(errors, window) {
+        test.equal(errors, null, 'errors should be null');
+        test.notEqual(window.document._cookie, null, 'window.document._cookie should not be null');
+        test.equal(window.document._cookie, cookie, 'window.document._cookie should match the configured value');
         test.done();
       }
     })
