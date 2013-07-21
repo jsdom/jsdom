28006d0c89cf1fd326c18ded7bd03da33fe15c89
Domenic Denicola <domenic@domenicdenicola.com>
2013-07-21 01:11:17 -0400

    Clean up some `env` tests.

--- test/jsdom/env.js
+++ test/jsdom/env.js
@@ -6,6 +6,14 @@ var http = require("http");
 var fs = require("fs");
 var toFileUrl = require("../util").toFileUrl(__dirname);
 
+exports["with invalid arguments"] = function (t) {
+  t.throws(function () { jsdom.env(); });
+  t.throws(function () { jsdom.env({}); });
+  t.throws(function () { jsdom.env({ html: "abc123" }); });
+  t.throws(function () { jsdom.env({ done: function () {} }); });
+  t.done();
+};
+
 exports["explicit config.html, full document"] = function (t) {
   env({
     html: "<!DOCTYPE html><html><head><title>Hi</title></head><body>Hello</body></html>",
@@ -19,7 +27,7 @@ exports["explicit config.html, full document"] = function (t) {
   });
 };
 
-exports["explicit config.html, just a string"] = function (t) {
+exports["explicit config.html, with overriden config.url"] = function (t) {
   env({
     html: "Hello",
     url: "http://example.com/",
@@ -27,6 +35,55 @@ exports["explicit config.html, just a string"] = function (t) {
       t.ifError(err);
       t.equal(window.document.innerHTML, "<html><body>Hello</body></html>");
       t.equal(window.location.href, "http://example.com/");
+      t.equal(window.location.search, "");
+      t.equal(window.location.hash, "");
+      t.done();
+    }
+  });
+};
+
+exports["explicit config.html, with overriden config.url including hash"] = function (t) {
+  env({
+    html: "Hello",
+    url: "http://example.com/#foo",
+    done: function (err, window) {
+      t.ifError(err);
+      t.equal(window.location.hash, "#foo");
+      t.done();
+    }
+  });
+};
+
+exports["explicit config.html, with overriden config.url including search and hash"] = function (t) {
+  env({
+    html: "Hello",
+    url: "http://example.com/?foo=bar#baz",
+    done: function (err, window) {
+      t.ifError(err);
+      t.equal(window.location.search, "?foo=bar");
+      t.equal(window.location.hash, "#baz");
+      t.done();
+    }
+  });
+};
+
+exports["explicit config.html, without a config.url"] = function (t) {
+  env({
+    html: "<html><body><p>hello world!</p></body></html>",
+    done: function (err, window) {
+      t.ifError(err);
+      t.notEqual(window.location.href, null);
+      t.done();
+    }
+  });
+};
+
+exports["explicit config.html, with poorly formatted HTML"] = function (t) {
+  env({
+    html: "some poorly<div>formed<b>html</div> fragment",
+    done: function (err, window) {
+      t.ifError(err);
+      t.notEqual(window.location.href, null);
       t.done();
     }
   });
@@ -119,6 +176,36 @@ exports["explicit config.file, invalid"] = function (t) {
   });
 };
 
+exports["explicit config.file, with a script"] = function (t) {
+  env({
+    file: path.resolve(__dirname, "files/env.html"),
+    scripts: [path.resolve(__dirname, "files/jquery.js")],
+    done: function (err, window) {
+      t.ifError(err);
+
+      var $ = window.jQuery;
+      var text = "Let's Rock!";
+
+      $("body").text(text);
+
+      t.equal($("body").text(), text);
+      t.done();
+    }
+  });
+};
+
+exports["explicit config.file, with spaces in the file name"] = function (t) {
+  var fileName = path.resolve(__dirname, "files/folder space/space.html");
+
+  env({
+    file: fileName,
+    done: function (err, window) {
+      t.ifError(err);
+      t.done();
+    }
+  });
+};
+
 exports["string, parseable as a URL, valid"] = function (t) {
   var html = "<html><head><title>Example URL</title></head><body>Example!</body></html>";
   var responseText = "<!DOCTYPE html>" + html;
@@ -190,4 +277,86 @@ exports["string, full HTML document"] = function (t) {
       t.done();
     }
   );
+};
+
+exports["with a nonexistant script"] = function (t) {
+  env({
+    html: "<!DOCTYPE html><html><body><p>hello world!</p></body></html>",
+    scripts: ["path/to/invalid.js", "another/invalid.js"],
+    done: function (err, window) {
+      t.ok(err);
+      t.equal(err.length, 2);
+      t.ok(window.location.href);
+      t.done();
+    }
+  });
+};
+
+exports["with src"] = function (t) {
+  env({
+    html: "<!DOCTYPE html><html><body><p>hello world!</p></body></html>",
+    src: "window.attachedHere = 123;",
+    done: function (err, window) {
+      t.ifError(err);
+      t.ok(window.location.href);
+      t.equal(window.attachedHere, 123);
+      t.equal(window.document.getElementsByTagName("p").item(0).innerHTML, "hello world!");
+      t.done();
+    }
+  });
+};
+
+exports["with document referrer"] = function (t) {
+  env({
+    html: "<!DOCTYPE html><html><body><p>hello world!</p></body></html>",
+    document: { referrer: "https://github.com/tmpvar/jsdom" },
+    done: function (err, window) {
+      t.ifError(err);
+      t.equal(window.document.referrer, "https://github.com/tmpvar/jsdom");
+      t.done();
+    }
+  });
+};
+
+exports["with document cookie"] = function (t) {
+  var time = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  var cookie = "key=value; expires=" + time.toGMTString() + "; path=/";
+
+  env({
+    html: "<!DOCTYPE html><html><body><p>hello world!</p></body></html>",
+    document: { cookie: cookie },
+    done: function (err, window) {
+      t.ifError(err);
+      t.equal(window.document.cookie, cookie);
+      t.done();
+    }
+  });
+};
+
+exports["with scripts and content retrieved from URLs"] = function (t) {
+  var routes = {
+    "/js": "window.attachedHere = 123;",
+    "/html": "<a href='/path/to/hello'>World</a>"
+  };
+
+  var server = http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Length": routes[req.url].length });
+    res.end(routes[req.url]);
+  });
+
+  server.listen(64000, "127.0.0.1", function () {
+    env({
+      url: "http://127.0.0.1:64000/html",
+      scripts: "http://127.0.0.1:64000/js",
+      done: function (err, window) {
+        server.close();
+
+        t.ifError(err);
+        t.equal(window.location.href, "http://127.0.0.1:64000/html");
+        t.equal(window.attachedHere, 123);
+        t.equal(window.document.getElementsByTagName("a").item(0).innerHTML, "World");
+        t.done();
+      }
+    });
+  });
 };
