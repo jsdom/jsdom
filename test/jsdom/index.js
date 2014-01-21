5fdcbbc50d5c0b541368ad966951503374de86eb
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-01-21 14:26:44 -0500

    Add a try catch statement to prevent the test suite from dying silently.

--- test/jsdom/index.js
+++ test/jsdom/index.js
@@ -64,10 +64,17 @@ exports.tests = {
     var window = jsdom.jsdom().createWindow();
 
     jsdom.jQueryify(window, "http://code.jquery.com/jquery.js", function () {
-      window.$("body").append('<html><body><div data-foo="bar"/><div data-baz="foo"/></body></html>');
+      try {
+          window.$("body").append('<html><body><div data-foo="bar"/><div data-baz="foo"/></body></html>');
 
-      test.equal(window.$('*[data-foo]').length, 1);
-      test.done();
+          test.equal(window.$('*[data-foo]').length, 1);
+          test.done();
+      }
+      catch(ex) {
+          console.log(ex);
+          test.ok(false, "Encountered an error");
+          test.done();
+      }
     });
   },
 
