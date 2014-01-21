c31139f1e05b86ae845e3c430f07b6d23980f0c7
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-01-21 10:53:00 -0500

    Added a whatwg suite to test the attribute ordering semantics specified at http://dom.spec.whatwg.org.

--- test/whatwg/attributes.js
+++ test/whatwg/attributes.js
@@ -1,133 +1,113 @@
-var testcase = require('nodeunit').testCase;
-var fs = require('fs');
-var path = require('path');
-var jsdom = require("../../lib/jsdom");
-var toFileUrl = require("../util").toFileUrl(__dirname);
+"use strict";
 
-var fileCache = {};
-var load = function(name, options) {
-  if (!options) {
-    options = {};
-  }
+var testcase = require("nodeunit").testCase;
+var load = require("../util").load(__dirname);
 
-  var file = path.resolve(__dirname, "files/" + name + ".html");
+exports["setAttribute should change the first attribute, irrespective of namespace"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-  if(!options.url) {
-    options.url = toFileUrl(file);
-  }
+  // setAttribute changes the first attribute, irrespective of namespace.
+  body.setAttributeNS("foo", "x", "first");
+  body.setAttributeNS("foo2", "x", "second");
+  body.setAttribute("x", "changed");
 
-  var contents = fileCache[file] || fs.readFileSync(file, 'utf8'),
-      doc      = jsdom.jsdom(null, null, options),
-      window   = doc.createWindow();
+  // Check that the attribues are as we expect.
+  test.equal(body.attributes.length, 2, "two attributes");
+  test.equal(body.getAttribute("x"), "changed");
+  test.equal(body.getAttributeNS("foo", "x"), "changed");
+  test.equal(body.getAttributeNS("foo2", "x"), "second");
 
-  doc.parent = window;
-  window.loadComplete = function() {};
-
-  doc.innerHTML = contents;
-  fileCache[file] = contents;
-  return doc;
+  test.done();
 };
 
-exports.tests = {
-  setAttribute01: function (test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+exports["removeAttribute should remove the first attribute, irrespective of namespace when the first attribute is not in a namespace"] = function(test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    // setAttribute changes the first attribute, irrespective of namespace.
-    body.setAttributeNS("foo", "x", "first");
-    body.setAttributeNS("foo2", "x", "second");
-    body.setAttribute("x", "changed");
+  // Set body with attributes for testing.
+  body.setAttribute("x", "first");
+  body.setAttributeNS("foo", "x", "second");
 
-    // Check that the attribues are as we expect.
-    test.equal(body.attributes.length, 2, "two attributes");
-    test.equal(body.getAttribute("x"), "changed");
-    test.equal(body.getAttributeNS("foo", "x"), "changed");
-    test.equal(body.getAttributeNS("foo2", "x"), "second");
+  // Check that the attribues are as we expect.
+  test.equal(body.attributes.length, 2, "two attributes");
+  test.equal(body.getAttribute("x"), "first");
+  test.equal(body.getAttributeNS(null, "x"), "first");
+  test.equal(body.getAttributeNS("foo", "x"), "second");
 
-    test.done();
-  },
-  removeAttribute01: function(test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+  // removeAttribute removes the first attribute with name "x" that
+  // we set on the element, irrespective of namespace.
+  body.removeAttribute("x");
 
-    // Set body with attributes for testing.
-    body.setAttribute("x", "first");
-    body.setAttributeNS("foo", "x", "second");
+  // The only attribute remaining should be the second one.
+  test.equal(body.getAttribute("x"), "second");
+  test.equal(body.getAttributeNS(null, "x"), null);
+  test.equal(body.getAttributeNS("foo", "x"), "second");
+  test.equal(body.attributes.length, 1, "one attribute");
 
-    // Check that the attribues are as we expect.
-    test.equal(body.attributes.length, 2, "two attributes");
-    test.equal(body.getAttribute("x"), "first");
-    test.equal(body.getAttributeNS(null, "x"), "first");
-    test.equal(body.getAttributeNS("foo", "x"), "second");
+  test.done();
+};
 
-    // removeAttribute removes the first attribute with name "x" that
-    // we set on the element, irrespective of namespace.
-    body.removeAttribute("x");
+exports["removeAttribute should remove the first attribute, irrespective of namespace when the first attribute is in a namespace"] = function(test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    // The only attribute remaining should be the second one.
-    test.equal(body.getAttribute("x"), "second");
-    test.equal(body.getAttributeNS(null, "x"), null);
-    test.equal(body.getAttributeNS("foo", "x"), "second");
-    test.equal(body.attributes.length, 1, "one attribute");
+  // Set body with attributes for testing.
+  body.setAttributeNS("foo", "x", "first");
+  body.setAttributeNS("foo2", "x", "second");
 
-    test.done();
-  },
-  removeAttribute02: function(test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+  // Check that the attribues are as we expect.
+  test.equal(body.attributes.length, 2, "two attributes");
+  test.equal(body.getAttribute("x"), "first");
+  test.equal(body.getAttributeNS("foo", "x"), "first");
+  test.equal(body.getAttributeNS("foo2", "x"), "second");
 
-    // Set body with attributes for testing.
-    body.setAttributeNS("foo", "x", "first");
-    body.setAttributeNS("foo2", "x", "second");
+  // removeAttribute removes the first attribute with name "x" that
+  // we set on the element, irrespective of namespace.
+  body.removeAttribute("x");
 
-    // Check that the attribues are as we expect.
-    test.equal(body.attributes.length, 2, "two attributes");
-    test.equal(body.getAttribute("x"), "first");
-    test.equal(body.getAttributeNS("foo", "x"), "first");
-    test.equal(body.getAttributeNS("foo2", "x"), "second");
+  // The only attribute remaining should be the second one.
+  test.equal(body.getAttribute("x"), "second");
+  test.equal(body.getAttributeNS("foo", "x"), null);
+  test.equal(body.getAttributeNS("foo2", "x"), "second");
+  test.equal(body.attributes.length, 1, "one attribute");
 
-    // removeAttribute removes the first attribute with name "x" that
-    // we set on the element, irrespective of namespace.
-    body.removeAttribute("x");
+  test.done();
+};
 
-    // The only attribute remaining should be the second one.
-    test.equal(body.getAttribute("x"), "second");
-    test.equal(body.getAttributeNS("foo", "x"), null);
-    test.equal(body.getAttributeNS("foo2", "x"), "second");
-    test.equal(body.attributes.length, 1, "one attribute");
+exports["hasAttribute should check for attribute presence, irrespective of namespace"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    test.done();
-  },
-  hasAttribute01: function(test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+  // Set body with attributes for testing.
+  body.setAttributeNS("foo", "x", "first");
 
-    // Set body with attributes for testing.
-    body.setAttributeNS("foo", "x", "first");
+  // Checks for attribute presence, irrespective of namespace.
+  test.ok(body.hasAttribute("x"));
 
-    // Checks for attribute presence, irrespective of namespace.
-    test.ok(body.hasAttribute("x"));
+  test.done();
+};
 
-    test.done();
-  },
-  indexing01: function (test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+exports["an attribute set by setAttributeNS should be accessible as a field on the `attributes` field of an Element"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    // This attribute should be available as body.attribute.x.
-    body.setAttribute("x", "first");
+  // This attribute should be available as body.attribute.x.
+  body.setAttribute("x", "first");
 
-    test.equal(body.attributes.length, 1, "one attribute");
-    test.equal(body.attributes.x.value, "first");
+  test.equal(body.attributes.length, 1, "one attribute");
+  test.equal(body.attributes.x.value, "first");
 
-    test.done();
-  },
-  indexing02: function (test) {
+  test.done();
+};
+
+
+exports["an attribute with a null namespace should be accessible as a field on the `attributes` field of an Element"] = function (test) {
     var doc = load("minimal");
     var body = doc.getElementsByTagName("body")[0];
     test.ok(body, "body is not null");
@@ -139,55 +119,58 @@ exports.tests = {
     test.equal(body.attributes.x.value, "first");
 
     test.done();
-  },
-  indexing03: function (test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+};
 
-    // This does not make body.attributes.x available.
-    body.setAttributeNS("foo", "x", "first");
+exports["an attribute with a set namespace should be accessible as a field on the `attributes` field of an Element"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    test.equal(body.attributes.length, 1, "one attribute");
-    test.equal(body.attributes.x, undefined);
+  // This does not make body.attributes.x available.
+  body.setAttributeNS("foo", "x", "first");
 
-    test.done();
-  },
-  indexing04: function (test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+  test.equal(body.attributes.length, 1, "one attribute");
+  test.equal(body.attributes.x, undefined);
 
-    // This does not overwrite methods.
-    body.setAttributeNS("foo", "setNamedItem", "first");
+  test.done();
+};
 
-    test.equal(body.attributes.length, 1, "one attribute");
-    test.equal(typeof body.attributes.setNamedItem, "function");
+exports["setting an attribute should not overwrite the methods of an `AttributeList` object"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    test.done();
-  },
-  indexing05: function (test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+  // This does not overwrite methods.
+  body.setAttributeNS("foo", "setNamedItem", "first");
 
-    // This does not overwrite methods on parent prototypes.
-    body.setAttributeNS("foo", "toString", "first");
+  test.equal(body.attributes.length, 1, "one attribute");
+  test.equal(typeof body.attributes.setNamedItem, "function");
 
-    test.equal(body.attributes.length, 1, "one attribute");
-    test.equal(typeof body.attributes.toString, "function");
+  test.done();
+};
 
-    test.done();
-  },
-  indexing06: function (test) {
-    var doc = load("minimal");
-    var body = doc.getElementsByTagName("body")[0];
-    test.ok(body, "body is not null");
+exports["setting an attribute should not overwrite the methods defined by parents of an `AttributeList` object"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
 
-    // This does not overwrite fields.
-    body.setAttributeNS("foo", "length", "first");
+  // This does not overwrite methods on parent prototypes.
+  body.setAttributeNS("foo", "toString", "first");
 
-    test.equal(body.attributes.length, 1, "one attribute");
-    test.done();
-  }
+  test.equal(body.attributes.length, 1, "one attribute");
+  test.equal(typeof body.attributes.toString, "function");
+
+  test.done();
+};
+
+exports["setting an attribute should not overwrite the fields of an `AttributeList` object"] = function (test) {
+  var doc = load("minimal");
+  var body = doc.getElementsByTagName("body")[0];
+  test.ok(body, "body is not null");
+
+  // This does not overwrite fields.
+  body.setAttributeNS("foo", "length", "first");
+
+  test.equal(body.attributes.length, 1, "one attribute");
+  test.done();
 };
