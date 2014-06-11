213429efcc192140ac809e3eb896c407368be23c
Sebastian Mayr <sebmaster16@gmail.com>
2014-06-11 11:55:53 +0200

    Add test for cookieEnabled on navigator

---
+++ test/whatwg/navigator.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+
+var jsdom = require("../..");
+
+exports["cookieEnabled should be true on the navigator object"] = function (t) {
+  var window = jsdom.jsdom().createWindow();
+  t.strictEqual(window.navigator.cookieEnabled, true, "cookie enabled");
+  t.done();
+};
