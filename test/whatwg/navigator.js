3f4583a0f65b8b9c714d5eb58b47ab52a7df4ca2
Sebastian Mayr <sebmaster16@gmail.com>
2014-06-15 21:13:10 +0200

    Add tests to ensure navigator props are read-only

--- test/whatwg/navigator.js
+++ test/whatwg/navigator.js
@@ -7,3 +7,24 @@ exports["cookieEnabled should be true on the navigator object"] = function (t) {
   t.strictEqual(window.navigator.cookieEnabled, true, "cookie enabled");
   t.done();
 };
+
+exports["navigator properties should be read-only from the site"] = function (t) {
+  t.expect(5);
+
+  jsdom.env({
+    html: '<!doctype html><html><head><script type="text/javascript">navigator.userAgent = "test";navigator.platform = "test";navigator.appName = "test";navigator.appVersion = "test";</script></head><body></body></html>',
+    done: function (errors, window) {
+      t.ifError(errors);
+
+      t.notEqual(window.navigator.userAgent, 'test', 'navigator.userAgent shouldn\'t be modifiable');
+      t.notEqual(window.navigator.platform, 'test', 'navigator.platform shouldn\'t be modifiable');
+      t.notEqual(window.navigator.appName, 'test', 'navigator.appName shouldn\'t be modifiable');
+      t.notEqual(window.navigator.appVersion, 'test', 'navigator.appVersion shouldn\'t be modifiable');
+      t.done();
+    },
+    features: {
+      FetchExternalResources: ['script'],
+      ProcessExternalResources:['script']
+    }
+  });
+};
