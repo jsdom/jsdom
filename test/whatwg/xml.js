e9c927fed2a34a4efa7c7105ada79c008f0ec5ba
Sebastian Mayr <sebmaster16@gmail.com>
2014-06-16 10:58:44 +0200

    Add test to ensure svg compatibility
    Up until now when provided an SVG document, jsdom.html always wrapped
    the content in a <html><body> construct. This should not happen in xml
    documents however.

---
+++ test/whatwg/xml.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+var jsdom = require("../..");
+var fs = require('fs');
+
+exports["should recognize svg and not wrap in <body> (GH-779)"] = function (t) {
+  var window = jsdom.env({
+    html: fs.readFileSync(__dirname + '/files/svg.svg'),
+    done: function (errs, window) {
+      t.ok(window.document.outerHTML.indexOf('<body>') === -1, 'should not contain body');
+      t.done();
+    }
+  });
+};
