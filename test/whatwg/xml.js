dd7358f8815816839edf79b2a6c214d22001e1ce
Sebastian Mayr <sebmaster16@gmail.com>
2014-06-18 21:35:30 +0200

    Add test for appending scripts to svg doc

--- test/whatwg/xml.js
+++ test/whatwg/xml.js
@@ -12,3 +12,17 @@ exports["should recognize svg and not wrap in <body> (GH-779)"] = function (t) {
     }
   });
 };
+
+
+exports["should allow inserting scripts into svg"] = function (t) {
+  var window = jsdom.env({
+    html: fs.readFileSync(__dirname + '/files/svg.svg'),
+    scripts: [__dirname + '/../jquery-fixtures/jquery-1.11.0.js'],
+    done: function (errs, window) {
+      t.ifError(errs);
+
+      t.notEqual(window.$, undefined, 'should load jQuery');
+      t.done();
+    }
+  });
+};
