3a2e207ac934a7438afc7799b90bd4c4e4aaaf43
Sebastian Mayr <sebmaster16@gmail.com>
2014-06-20 00:55:13 +0200

    Add easy method to execute the w3c web platform tests

---
+++ test/w3c/index.js
@@ -0,0 +1,6 @@
+﻿"use strict";
+
+var w3cTest = require("./w3ctest")(exports);
+
+w3cTest("http://w3c-test.org/dom/nodes/Node-cloneNode.html");
+w3cTest("http://w3c-test.org/dom/nodes/Document-createComment.html");
