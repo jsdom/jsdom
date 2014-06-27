cdd79dc14f47299a63863c19ed38a60a7f99c91c
Michael Hayes <michael.hayes@urbanairship.com>
2014-06-26 18:13:08 -0700

    Fix textContent for nodes that do not have children
    Fixes #483.

---
+++ test/level3/textContent.js
@@ -0,0 +1,27 @@
+var hc_staff = require("./core/files/hc_staff.xml");
+
+// Most of the level3 tests fail, so we don't turn them on by default. We extracted out one that passes into this file.
+
+exports.tests = {
+  nodesettextcontent07: function (test) {
+    var success;
+    var doc;
+    var elemList;
+    var elem;
+    var txt;
+    var textContent;
+
+    doc = hc_staff.hc_staff();
+    elemList = doc.getElementsByTagName("em");
+    elem = elemList.item(0);
+    txt = elem.firstChild;
+
+    txt.textContent = "Text";
+
+    textContent = txt.textContent;
+
+    test.equal(textContent, "Text", 'nodegettextcontent10');
+
+    test.done()
+  },
+}
