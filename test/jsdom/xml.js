ef4718372572b009313ee8141cb8ecf376893d3d
Sebastian Mayr <sebmaster16@gmail.com>
2014-09-17 21:43:43 +0100

    Factor xml source into its own file

--- test/jsdom/xml.js
+++ test/jsdom/xml.js
@@ -1,6 +1,8 @@
 ﻿"use strict";
 
+var fs = require("fs");
 var jsdom = require("../..");
+var xmlString = fs.readFileSync(__dirname + "/files/xml.xml");
 
 exports["should not throw if no parser is given"] = function (t) {
   var document = jsdom.jsdom("<!DOCTYPE html><html></html>");
@@ -15,33 +17,29 @@ exports["should not throw if invalid html document is given"] = function (t) {
 };
 
 exports["should ignore self-closing of tags in html docs"] = function (t) {
-  var document = jsdom.jsdom(
-    "<?xml version='1.0' encoding='utf-8'?><html xmlns='http://www.w3.org/1999/xhtml'><head><title>T</title></head><body><CUSTOMTAG />a</body></html>",
-    { parsingMode: "html" });
+  var document = jsdom.jsdom(xmlString, { parsingMode: "html" });
 
   var window = document.parentWindow;
-  t.strictEqual(window.document.getElementsByTagName("CUSTOMTAG")[0].innerHTML, "a");
+  t.strictEqual(window.document.getElementsByTagName("CUSTOMTAG")[0].innerHTML.trim(), "a");
 
   t.done();
 };
 
 exports["should handle self-closing tags properly in xml docs (in .jsdom)"] = function (t) {
-  var document = jsdom.jsdom(
-    "<?xml version='1.0' encoding='utf-8'?><html xmlns='http://www.w3.org/1999/xhtml'><head><title>T</title></head><body><CUSTOMTAG />a</body></html>",
-    { parsingMode: "xml" });
+  var document = jsdom.jsdom(xmlString, { parsingMode: "xml" });
 
   var window = document.parentWindow;
-  t.strictEqual(window.document.getElementsByTagName("CUSTOMTAG")[0].innerHTML, "");
+  t.strictEqual(window.document.getElementsByTagName("CUSTOMTAG")[0].innerHTML.trim(), "");
 
   t.done();
 };
 
 exports["should handle self-closing tags properly in xml docs (in .env)"] = function (t) {
   var document = jsdom.env({
-    html: "<?xml version='1.0' encoding='utf-8'?><html xmlns='http://www.w3.org/1999/xhtml'><head><title>T</title></head><body><CUSTOMTAG />a</body></html>",
+    html: xmlString,
     parsingMode: "xml",
     done: function (err, window) {
-      t.strictEqual(window.document.getElementsByTagName("CUSTOMTAG")[0].innerHTML, "");
+      t.strictEqual(window.document.getElementsByTagName("CUSTOMTAG")[0].innerHTML.trim(), "");
 
       t.done();
     }
