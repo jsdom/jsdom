f25e41e4f03c11f2f504211bc7a4175da939a826
Sebastian Mayr <sebmaster16@gmail.com>
2014-09-23 12:40:40 +0100

    Add saucelabs configuration

---
+++ test/ci.js
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env node
+
+"use strict";
+
+if (process.env.TEST_SUITE === "node") {
+  require("./runner");
+} else if (process.env.TEST_SUITE === "browser") {
+  require("./browser-runner");
+} else {
+  process.exit(1);
+}
