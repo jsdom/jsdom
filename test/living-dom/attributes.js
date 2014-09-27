253aeb4c287083d605df61752ab283089c163b9c
Sebastian Mayr <sebmaster16@gmail.com>
2014-09-27 01:21:53 +0100

    Fix handling of value property on inputs

--- test/living-dom/attributes.js
+++ test/living-dom/attributes.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var jsdom = require("../..");
 var load = require("../util").load(__dirname);
 
 exports["setAttribute should change the first attribute, irrespective of namespace"] = function (t) {
@@ -177,5 +178,44 @@ exports["setting an attribute should not overwrite the fields of an `AttributeLi
   body.setAttributeNS("foo", "length", "first");
 
   t.equal(body.attributes.length, 1, "one attribute");
+  t.done();
+};
+
+exports["html input should handle value/defaultValue correctly"] = function (t) {
+  var input = jsdom.jsdom("<input>").querySelector("input");
+
+  t.strictEqual(input.value, "",
+    "value should equal empty string if uninitialized");
+  t.strictEqual(input.defaultValue, "",
+    "defaultValue should equal empty string if uninitialized");
+  t.strictEqual(input.getAttribute("value"), null,
+    "value attribute should be null (uninitialized)");
+
+  input.defaultValue = "abc";
+
+  t.strictEqual(input.value, "abc",
+    "value should default to defaultValue if undefined");
+  t.strictEqual(input.defaultValue, "abc",
+    "defaultValue should equal to set string");
+  t.strictEqual(input.getAttribute("value"), "abc",
+    "value attribute should equal to set string");
+
+  input.value = "def";
+
+  t.strictEqual(input.value, "def",
+    "value should get changed by setter");
+  t.strictEqual(input.defaultValue, "abc",
+    "defaultValue should equal to set string");
+  t.strictEqual(input.getAttribute("value"), "abc",
+    "value attribute should not change");
+
+  input.value = null;
+
+  t.strictEqual(input.value, "abc",
+    "resetting value should fall back to defaultValue");
+  t.strictEqual(input.getAttribute("value"), "abc",
+    "value attribute should not change");
+
+
   t.done();
 };
