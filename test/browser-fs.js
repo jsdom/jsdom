6c3ae04ea228cc970021805d1e6ce3a7e1c07d18
Sebastian Mayr <sebmaster16@gmail.com>
2014-10-13 19:08:23 +0100

    Fix problems with the load() util used in tests
    This works by emulating readFileSync by making a synchronous XHR.

---
+++ test/browser-fs.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+exports.readFileSync = function (file, opts) {
+  if (opts !== "utf8" && opts !== "utf-8") throw new Error("fs without utf8 encoding is not supported!");
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", file, false);
+  xhr.send(null);
+
+  return xhr.responseText;
+};
