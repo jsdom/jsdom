37728a01e5e61d7eb8cd858094d173cd206bc8f1
Frederic Hemberger <mail@frederic-hemberger.de>
2014-10-25 14:22:16 +0200

    Add document.implementation.createHTMLDocument()

--- test/w3c/index.js
+++ test/w3c/index.js
@@ -4,4 +4,6 @@ var w3cTest = require("./w3ctest")(exports);
 
 w3cTest("dom/nodes/Node-cloneNode.html");
 w3cTest("dom/nodes/Document-createComment.html");
+w3cTest("dom/nodes/DOMImplementation-createHTMLDocument.html");
+w3cTest("dom/nodes/Document-contentType/contentType/createHTMLDocument.html");
 w3cTest("dom/traversal/NodeFilter-constants.html");
