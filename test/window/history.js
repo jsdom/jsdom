6697896e265fa58475a62692e134664eba9e87c4
Ryan Grove <ryan@wonko.com>
2014-11-04 12:39:33 -0800

    Initial history.length should be 1, not 0.
    This brings jsdom in line with both the spec and with browsers, and fixes an issue
    where calling `history.pushState()` and then `history.back()` would fail to trigger a
    `popstate` event for the `back()` call, since there was no initial history entry to
    navigate back to.
    
    From https://html.spec.whatwg.org/multipage/browsers.html#browsing-context:
    
    "When a browsing context is first created, it must be created with a single Document in its session history..."

--- test/window/history.js
+++ test/window/history.js
@@ -7,7 +7,7 @@ exports["a default window should have a history object with correct default valu
 
   t.ok(window.history);
   t.strictEqual(window.history.state, null);
-  t.strictEqual(window.history.length, 0);
+  t.strictEqual(window.history.length, 1);
 
   t.done();
 };
@@ -17,26 +17,26 @@ exports["the history object should update correctly when calling pushState/repla
 
   // Absolute path
   window.history.pushState({ foo: "one" }, "unused title", "/bar/baz#fuzz");
-  t.strictEqual(window.history.length, 1);
+  t.strictEqual(window.history.length, 2);
   t.strictEqual(window.history.state.foo, "one");
   t.strictEqual(window.location.pathname, "/bar/baz");
   t.strictEqual(window.location.hash, "#fuzz");
 
   window.history.pushState({ foo: "two" }, "unused title 2", "/bar/foo#boo");
-  t.strictEqual(window.history.length, 2);
+  t.strictEqual(window.history.length, 3);
   t.strictEqual(window.history.state.foo, "two");
   t.strictEqual(window.location.pathname, "/bar/foo");
   t.strictEqual(window.location.hash, "#boo");
 
   // Relative path
   window.history.pushState({ foo: "three" }, "unused title 3", "fizz");
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "three");
   t.strictEqual(window.location.pathname, "/bar/fizz");
   t.strictEqual(window.location.hash, "");
 
   window.history.replaceState({ foo: "four" }, "unused title 4", "/buzz");
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "four");
   t.strictEqual(window.location.pathname, "/buzz");
 
@@ -45,6 +45,7 @@ exports["the history object should update correctly when calling pushState/repla
 
 exports["the history object should update correctly when calling forward/back/go"] = function (t) {
   var window = jsdom().parentWindow;
+  var initialPath = window.location.pathname;
 
   [
     [{ foo: "bar" }, "title 1", "/bar"],
@@ -55,36 +56,41 @@ exports["the history object should update correctly when calling forward/back/go
   });
 
   // Sanity check
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "buzz");
   t.strictEqual(window.location.pathname, "/buzz");
 
   // Test forward boundary
   window.history.forward();
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "buzz");
   t.strictEqual(window.location.pathname, "/buzz");
 
   window.history.back();
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "baz");
   t.strictEqual(window.location.pathname, "/baz");
 
   window.history.back();
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "bar");
   t.strictEqual(window.location.pathname, "/bar");
+
+  window.history.back();
+  t.strictEqual(window.history.length, 4);
+  t.strictEqual(window.history.state, null);
+  t.strictEqual(window.location.pathname, initialPath);
 
   // Test backward boundary
   window.history.back();
-  t.strictEqual(window.history.length, 3);
-  t.strictEqual(window.history.state.foo, "bar");
-  t.strictEqual(window.location.pathname, "/bar");
+  t.strictEqual(window.history.length, 4);
+  t.strictEqual(window.history.state, null);
+  t.strictEqual(window.location.pathname, initialPath);
 
   window.history.go(2);
-  t.strictEqual(window.history.length, 3);
-  t.strictEqual(window.history.state.foo, "buzz");
-  t.strictEqual(window.location.pathname, "/buzz");
+  t.strictEqual(window.history.length, 4);
+  t.strictEqual(window.history.state.foo, "baz");
+  t.strictEqual(window.location.pathname, "/baz");
 
   t.done();
 };
@@ -101,19 +107,19 @@ exports["the history object should update correctly when calling pushState with 
   });
 
   // Sanity check
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "buzz");
   t.strictEqual(window.location.pathname, "/buzz");
   window.history.go(-2);
 
-  t.strictEqual(window.history.length, 3);
+  t.strictEqual(window.history.length, 4);
   t.strictEqual(window.history.state.foo, "bar");
   t.strictEqual(window.location.pathname, "/bar");
 
   // Call pushState when index is behind length
   window.history.pushState({ foo: "bar-b" }, "title 2b", "/bar/b");
 
-  t.strictEqual(window.history.length, 2);
+  t.strictEqual(window.history.length, 3);
   t.strictEqual(window.history.state.foo, "bar-b");
   t.strictEqual(window.location.pathname, "/bar/b");
 
