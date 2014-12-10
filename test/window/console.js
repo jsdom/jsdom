3a32b36068baf2bce9218cd869e5e1f3ba4d607f
Jeff Carpenter <gcarpenterv@gmail.com>
2014-12-10 07:57:58 +0000

    Add missing window.console methods

--- test/window/console.js
+++ test/window/console.js
@@ -2,6 +2,37 @@
 
 var jsdom = require("../..").jsdom;
 
+// Tests for window.console
+// Spec: https://terinjokes.github.io/console-spec/
+
+exports["console has all methods in spec"] = function (t) {
+  var window = jsdom().parentWindow;
+
+  var methods = [
+    'assert',
+    'clear',
+    'count',
+    'debug',
+    'error',
+    'info',
+    'log',
+    'table',
+    'trace',
+    'warn',
+    'group',
+    'groupCollapse',
+    'groupEnd',
+    'time',
+    'timeEnd'
+  ];
+
+  methods.forEach(function (method) {
+    t.ok(typeof window.console[method] === "function", "console has the " + method + " method");
+  });
+
+  t.done();
+};
+
 exports["only console.error should put errors in the window.document.errors array"] = function (t) {
   var window = jsdom("<!DOCTYPE html><html><body>Hi</body></html>").parentWindow;
 
