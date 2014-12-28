4de8752d25031603632b1abf74e8f575469123eb
Domenic Denicola <d@domenic.me>
2014-12-27 20:00:59 -0500

    Move selectors code to lib/jsdom/living
    Also fleshed out the tests, and made querySelector correctly stringify its argument.

--- test/living-dom/query-selector-all.js
+++ test/living-dom/query-selector-all.js
@@ -1,17 +1,44 @@
 "use strict";
 
+var jsdom = require("../..").jsdom;
 var load = require("../util").load(__dirname);
 
-// Tests for ParentNode's querySelector
+// Tests for ParentNode's querySelectorAll
 // Spec: https://dom.spec.whatwg.org/#dom-parentnode-queryselectorall
 
-exports["querySelectorAll exists on any given node"] = function (t) {
+exports["querySelectorAll exists on documents"] = function (t) {
   var doc = load("test");
 
   t.ok(doc.querySelectorAll, "document.querySelectorAll exists");
   t.ok(typeof doc.querySelectorAll === "function", "document.querySelectorAll is a function");
-  t.ok(doc.querySelectorAll("body"), "document.querySelectorAll can find the body element");
-  t.ok(doc.querySelectorAll("p"), "document.querySelectorAll can find a 'p' element");
+  t.ok(doc.querySelectorAll("body"), "document.querySelectorAll can find the <body> element");
+  t.strictEqual(doc.querySelectorAll("p").length, 1, "document.querySelectorAll can find a <p> element");
+
+  t.done();
+};
+
+exports["querySelectorAll exists on elements"] = function (t) {
+  var doc = load("test");
+
+  t.ok(doc.body.querySelectorAll, "document.body.querySelectorAll exists");
+  t.ok(typeof doc.body.querySelectorAll === "function", "document.body.querySelectorAll is a function");
+  t.strictEqual(doc.body.querySelectorAll("p").length, 1, "document.querySelectorAll can find a <p> element");
+
+  t.done();
+};
+
+exports["querySelectorAll exists on document fragments"] = function (t) {
+  var doc = jsdom();
+  var docFrag = doc.createDocumentFragment();
+
+  var div = doc.createElement("div");
+  div.innerHTML = "<p>Hello</p>";
+  docFrag.appendChild(div);
+
+  t.ok(docFrag.querySelectorAll, "docFrag.querySelectorAll exists");
+  t.ok(typeof docFrag.querySelectorAll === "function", "docFrag.querySelectorAll is a function");
+  t.strictEqual(docFrag.querySelectorAll("div").length, 1, "document.querySelectorAll can find a <div> element");
+  t.strictEqual(docFrag.querySelectorAll("p").length, 1, "document.querySelectorAll can find a <p> element");
 
   t.done();
 };
--- test/living-dom/query-selector.js
+++ test/living-dom/query-selector.js
@@ -1,17 +1,56 @@
 "use strict";
 
+var jsdom = require("../..").jsdom;
 var load = require("../util").load(__dirname);
 
 // Tests for ParentNode's querySelector
 // Spec: https://dom.spec.whatwg.org/#dom-parentnode-queryselector
 
-exports["querySelector exists on any given node"] = function (t) {
+exports["querySelector exists on documents"] = function (t) {
   var doc = load("test");
 
   t.ok(doc.querySelector, "document.querySelector exists");
   t.ok(typeof doc.querySelector === "function", "document.querySelector is a function");
-  t.ok(doc.querySelector("body"), "document.querySelector can find the body element");
-  t.ok(doc.querySelector("p"), "document.querySelector can find a 'p' element");
+  t.ok(doc.querySelector("body"), "document.querySelector can find the <body> element");
+  t.ok(doc.querySelector("p"), "document.querySelector can find a <p> element");
+
+  t.done();
+};
+
+exports["querySelector exists on elements"] = function (t) {
+  var doc = load("test");
+
+  t.ok(doc.body.querySelector, "document.body.querySelector exists");
+  t.ok(typeof doc.body.querySelector === "function", "document.body.querySelector is a function");
+  t.ok(doc.body.querySelector("p"), "document.body.querySelector can find a <p> element");
+
+  t.done();
+};
+
+exports["querySelector exists on document fragments"] = function (t) {
+  var doc = jsdom();
+  var docFrag = doc.createDocumentFragment();
+
+  var div = doc.createElement("div");
+  div.innerHTML = "<p>Hello</p>";
+  docFrag.appendChild(div);
+
+  t.ok(docFrag.querySelector, "docFrag.querySelector exists");
+  t.ok(typeof docFrag.querySelector === "function", "docFrag.querySelector is a function");
+  t.ok(docFrag.querySelector("div"), "document.querySelector can find a <div> element");
+  t.ok(docFrag.querySelector("p"), "document.querySelector can find a <p> element");
+
+  t.done();
+};
+
+exports["querySelector converts its argument to a string before processing"] = function (t) {
+  var doc = load("test");
+
+  var element = doc.querySelector(["strong"]);
+  t.ok(element, "document.querySelector returns the <strong> element");
+
+  var expectedP = doc.querySelector({ toString: function () { return "p"; } });
+  t.ok(expectedP, "document.querySelector calls toString on any given object");
 
   t.done();
 };
