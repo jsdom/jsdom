a5c7eb9054fc11e0807ac38a72892dd5cda468f3
Joris van der Wel <joris@jorisvanderwel.com>
2015-04-25 18:22:20 +0200

    new DOMImplementation() is not allowed
    This also required fixing the existing test cases, which uncovered a bug wherein doctype nodes are not properly adopted

--- test/living-dom/dom-implementation.js
+++ test/living-dom/dom-implementation.js
@@ -1,29 +1,39 @@
 "use strict";
 const jsdom = require("../..");
 
+exports["new DOMImplementation() is not allowed"] = function (t) {
+  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
+
+  let foo;
+  t.throws(function () {
+    foo = new DOMImplementation();
+  }, /Illegal constructor/i);
+
+  t.done();
+};
 
 exports["create an empty document"] = function (t) {
-  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
-  const dom = new DOMImplementation();
-  const document = dom.createDocument(null, null, null);
+  const implementation = jsdom.jsdom().implementation;
+  const document = implementation.createDocument(null, null, null);
   t.equal(document.childNodes.length, 0, "document should not contain any nodes");
   t.done();
 };
 
 exports["doctype ownerDocument"] = function (t) {
-  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
-  const dom = new DOMImplementation();
-  const doctype = dom.createDocumentType("bananas");
-  const document = dom.createDocument(null, null, doctype);
-  t.equal(doctype.ownerDocument, document, "doctype should belong to the document");
+  const document = jsdom.jsdom();
+  const doctype = document.implementation.createDocumentType("bananas");
+  t.ok(doctype.ownerDocument === document, "doctype should belong to the document the implementation belongs to");
+  const newDocument = document.implementation.createDocument(null, null, doctype);
+  t.ok(doctype.ownerDocument === newDocument, "doctype should belong to the new document");
   t.done();
 };
 
 exports["doctype child of ownerDocument"] = function (t) {
-  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
-  const dom = new DOMImplementation();
-  const doctype = dom.createDocumentType("hatstand");
-  const document = dom.createDocument(null, null, doctype);
-  t.equal(document.firstChild, doctype, "doctype should be a child of the document");
+  const document = jsdom.jsdom();
+  const doctype = document.implementation.createDocumentType("hatstand");
+  const newDocument = document.implementation.createDocument(null, null, doctype);
+  t.ok(newDocument.firstChild === doctype, "doctype should be a child of the document");
+  t.done();
+};
   t.done();
 };
