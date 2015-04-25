abfa5e0a47a5b0c63ce6157540694dd6b958c884
Joris van der Wel <joris@jorisvanderwel.com>
2015-04-25 18:02:57 +0200

    Use const instead of var

--- test/living-dom/dom-implementation.js
+++ test/living-dom/dom-implementation.js
@@ -1,29 +1,29 @@
 "use strict";
-var jsdom = require("../..");
+const jsdom = require("../..");
 
 
 exports["create an empty document"] = function (t) {
-  var DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
-  var dom = new DOMImplementation();
-  var document = dom.createDocument(null, null, null);
+  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
+  const dom = new DOMImplementation();
+  const document = dom.createDocument(null, null, null);
   t.equal(document.childNodes.length, 0, "document should not contain any nodes");
   t.done();
 };
 
 exports["doctype ownerDocument"] = function (t) {
-  var DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
-  var dom = new DOMImplementation();
-  var doctype = dom.createDocumentType("bananas");
-  var document = dom.createDocument(null, null, doctype);
+  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
+  const dom = new DOMImplementation();
+  const doctype = dom.createDocumentType("bananas");
+  const document = dom.createDocument(null, null, doctype);
   t.equal(doctype.ownerDocument, document, "doctype should belong to the document");
   t.done();
 };
 
 exports["doctype child of ownerDocument"] = function (t) {
-  var DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
-  var dom = new DOMImplementation();
-  var doctype = dom.createDocumentType("hatstand");
-  var document = dom.createDocument(null, null, doctype);
+  const DOMImplementation = jsdom.jsdom().defaultView.DOMImplementation;
+  const dom = new DOMImplementation();
+  const doctype = dom.createDocumentType("hatstand");
+  const document = dom.createDocument(null, null, doctype);
   t.equal(document.firstChild, doctype, "doctype should be a child of the document");
   t.done();
 };
