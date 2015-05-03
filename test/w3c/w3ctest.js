3afc4f3f16d63881d1798a3a28d04ad9e698d75d
Domenic Denicola <d@domenic.me>
2015-05-03 16:25:57 +0200

    Lint w3c test files that we write

--- test/w3c/w3ctest.js
+++ test/w3c/w3ctest.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 var request = require("request");
 var jsdom = require("../..");
 var resolveHref = require("../../lib/jsdom/utils").resolveHref;
 
 function createJsdom(source, url, t) {
-  const reporterHref = resolveHref(__dirname, 'w3c/tests/resources/testharnessreport.js');
+  const reporterHref = resolveHref(__dirname, "w3c/tests/resources/testharnessreport.js");
 
   jsdom.env({
     html: source,
@@ -18,18 +18,19 @@ function createJsdom(source, url, t) {
     },
     resourceLoader: function (resource, callback) {
       if (resource.url.href === reporterHref) {
-        callback(null, 'window.shimTest();');
+        callback(null, "window.shimTest();");
       } else {
         resource.defaultFetch(callback);
       }
     },
     created: function (err, window) {
       if (err) {
-        t.ifError(err, 'window should be created without error');
+        t.ifError(err, "window should be created without error");
         t.done();
       }
 
       window.shimTest = function () {
+        /* jshint -W106 */
         window.add_result_callback(function (test) {
           if (test.status === 1) {
             t.ok(false, "Failed in \"" + test.name + "\": \n" + test.message);
@@ -40,40 +41,40 @@ function createJsdom(source, url, t) {
           }
         });
 
-        window.add_completion_callback(function (tests, harness_status) {
-          t.ok(harness_status.status !== 2, "test harness should not timeout");
+        window.add_completion_callback(function (tests, harnessStatus) {
+          t.ok(harnessStatus.status !== 2, "test harness should not timeout");
           window.close();
           t.done();
         });
       };
     },
 
-    loaded: function (err, window) {
+    loaded: function (err) {
       t.ifError(err && err[0].data.error);
     }
   });
 }
 
 function testUrl(url, t) {
-  fs.readFile(path.resolve(__dirname, 'tests', url), 'utf8', function (err, file) {
+  fs.readFile(path.resolve(__dirname, "tests", url), "utf8", function (err, file) {
     if (err) {
-      request.get('http://w3c-test.org/' + url, function (err, resp, respBody) {
+      request.get("http://w3c-test.org/" + url, function (err, resp, respBody) {
         if (err) {
-          t.ifError(err, 'request should go through without error');
+          t.ifError(err, "request should go through without error");
           t.done();
           return;
         }
         if (resp.statusCode < 200 || resp.statusCode >= 300) {
-          t.ok(false, 'request should return OK status code');
+          t.ok(false, "request should return OK status code");
           t.done();
           return;
         }
 
-        createJsdom(respBody, 'http://w3c-test.org/' + url, t);
+        createJsdom(respBody, "http://w3c-test.org/" + url, t);
       });
     } else {
-      file = file.replace(/\/resources\//gi, __dirname + '/tests/resources/');
-      createJsdom(file, path.resolve(__dirname, 'tests', url), t);
+      file = file.replace(/\/resources\//gi, __dirname + "/tests/resources/");
+      createJsdom(file, path.resolve(__dirname, "tests", url), t);
     }
   });
 }
