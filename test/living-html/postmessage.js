49ce31a27ce19bf2b1d949b258b445893fc93af0
jeffcarp <gcarpenterv@gmail.com>
2015-05-29 14:26:16 -0700

    Add targetOrigin validation, add tests, refactor

--- test/living-html/postmessage.js
+++ test/living-html/postmessage.js
@@ -17,8 +17,38 @@ function wrapScriptInHtmlDocumentString(js) {
   `;
 }
 
-// Tests for window.postMessage
+const baseOptions = {
+  features: {
+    FetchExternalResources: ["script","iframe"],
+    ProcessExternalResources: ["script","iframe"]
+  }
+};
+
+// Tests for window.postMessage(message, targetOrigin, transfer)
 // Spec: https://html.spec.whatwg.org/#crossDocumentMessages
+
+exports["throws SyntaxError on invalid targetOrigin"] = function (test) {
+  var document = jsdom.jsdom(wrapScriptInHtmlDocumentString(`
+    window.iframe = document.createElement("iframe");
+    window.iframe.src = "${toFileUrl("files/blank.html")}";
+    document.body.appendChild(window.iframe);
+  `), baseOptions);
+  var window = document.defaultView;
+
+  window.iframe.onload = function () {
+    test.throws(function () {
+      window.iframe.postMessage("testMessage", "bogus targetOrigin");
+    }, SyntaxError, "an invalid targetOrigin throws a SyntaxError");
+
+    test.throws(function () {
+      window.iframe.postMessage("testMessage");
+    }, TypeError, "an missing targetOrigin throws a SyntaxError");
+
+    test.done();
+  };
+};
+
+// TODO: Test that it clones the message
 
 exports["postMessage from iframe to parent"] = function (test) {
   var html = wrapScriptInHtmlDocumentString(`
@@ -30,44 +60,55 @@ exports["postMessage from iframe to parent"] = function (test) {
     });
   `);
 
-  var options = {
-    features: {
-      FetchExternalResources: ["script","iframe"],
-      ProcessExternalResources: ["script","iframe"]
-    }
-  }
-
-  var document = jsdom.jsdom(html, options);
+  var document = jsdom.jsdom(html, baseOptions);
   var window = document.defaultView;
 
   window.iframe.onload = function() {
     var event = window.postMessageReceived;
-    test.ok(event.data === "yep");
+    test.ok(event.data === "ack");
     test.done();
   };
 };
 
-exports["postMessage from parent to iframe"] = function (test) {
+exports["postMessage an object from iframe to parent"] = function (test) {
+  var html = wrapScriptInHtmlDocumentString(`
+    window.iframe = document.createElement("iframe");
+    // TODO: make html file names consistent:
+    // iframe-sender sends to a sibling iframe, but
+    // iframe-sender-object sends to parent
+    // Ideally we could specify the src here
+    window.iframe.src = "${toFileUrl("files/iframe-sender-object.html")}";
+    document.body.appendChild(window.iframe);
+    window.addEventListener("message", function (event) {
+      window.postMessageReceived = event;
+    });
+  `);
 
-  // TODO: We can modify the old frame tests to use template strings now
+  var document = jsdom.jsdom(html, baseOptions);
+  var window = document.defaultView;
+
+//  window.iframe.onload = function() {
+  setTimeout(function () {
+    var event = window.postMessageReceived;
+    test.ok(typeof event.data === "object");
+    test.ok(event.data.foo === "bar");
+    test.done();
+  }, 1e3);
+};
+
+exports["postMessage from parent to iframe"] = function (test) {
+  test.expect(1);
   var html = wrapScriptInHtmlDocumentString(`
     window.iframe = document.createElement("iframe");
     window.iframe.src = "${toFileUrl("files/iframe-receiver.html")}";
     document.body.appendChild(window.iframe);
     window.iframe.onload = function () {
-      window.iframe.postMessage("ack");
+      window.iframe.postMessage("ack", "*");
       window.testCallback();
     };
   `);
 
-  var options = {
-    features: {
-      FetchExternalResources: ["script","iframe"],
-      ProcessExternalResources: ["script","iframe"]
-    }
-  }
-
-  var document = jsdom.jsdom(html, options);
+  var document = jsdom.jsdom(html, baseOptions);
   var window = document.defaultView;
 
   window.testCallback = function () {
@@ -78,8 +119,10 @@ exports["postMessage from parent to iframe"] = function (test) {
 };
 
 exports["postMessage from iframe to iframe"] = function (test) {
-
-  var html = wrapScriptInHtmlDocumentString(`
+  // TODO: either use test.expect for all or none
+  test.expect(1);
+  // TODO: is jsdom.jsdom inline like this easier to read? or the other way?
+  var document = jsdom.jsdom(wrapScriptInHtmlDocumentString(`
     window.iframeReceiver = document.createElement("iframe");
     window.iframeReceiver.src = "${toFileUrl("files/iframe-receiver.html")}";
     document.body.appendChild(window.iframeReceiver);
@@ -91,19 +134,7 @@ exports["postMessage from iframe to iframe"] = function (test) {
     window.onload = function () {
       window.testCallback();
     };
-  `);
-
-  var virtualConsole = jsdom.createVirtualConsole();
-  virtualConsole.sendTo(console);
-  var options = {
-    virtualConsole: virtualConsole,
-    features: {
-      FetchExternalResources: ["script","iframe"],
-      ProcessExternalResources: ["script","iframe"]
-    }
-  }
-
-  var document = jsdom.jsdom(html, options);
+  `), baseOptions);
   var window = document.defaultView;
 
   window.testCallback = function () {
