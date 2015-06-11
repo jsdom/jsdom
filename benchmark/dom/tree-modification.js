5605743cb89f4b4bf94c8df5d8b3a739ff8306b9
Joris van der Wel <joris@jorisvanderwel.com>
2015-06-12 01:22:57 +0200

    Benchmark appendChild and removeChild
    This demonstrates using a setup function
    
    (Also: the tests with many parents are extremely slow in jsdom)

---
+++ benchmark/dom/tree-modification.js
@@ -0,0 +1,167 @@
+"use strict";
+const suite = require("../document-suite");
+
+exports["appendChild: no siblings"] = function () {
+  let parent;
+  let children;
+  let it;
+
+  return suite({
+    setup: function (document) {
+      parent = [];
+      children = [];
+
+      for (let i = 0; i < this.count; ++i) {
+        parent.push(document.createElement("div"));
+        children.push(document.createElement("div"));
+      }
+
+      it = 0;
+    },
+    fn: function () {
+      parent[it].appendChild(children[it]);
+      ++it;
+    }
+  });
+};
+
+exports["appendChild: many siblings"] = function () {
+  let parent;
+  let children;
+  let it;
+
+  return suite({
+    setup: function (document) {
+      parent = document.createElement("div");
+      children = [];
+
+      for (let i = 0; i < this.count; ++i) {
+        children.push(document.createElement("div"));
+      }
+
+      it = 0;
+    },
+    fn: function () {
+      parent.appendChild(children[it]);
+      ++it;
+    }
+  });
+};
+
+exports["appendChild: many parents"] = function () {
+  const DEPTH = 500;
+  let itData;
+  let it;
+
+  return suite({
+    setup: function (document) {
+      itData = new Array(this.count);
+
+      for (let i = 0; i < this.count; ++i) {
+        let nodes = new Array(DEPTH);
+
+        for (let n = 0; n < DEPTH; ++n) {
+          nodes[n] = document.createElement("div");
+        }
+
+        itData[i] = nodes;
+      }
+
+      it = 0;
+    },
+    fn: function () {
+      let nodes = itData[it];
+
+      for (let n = 1; n < DEPTH; ++n) {
+        nodes[n - 1].appendChild(nodes[n]);
+      }
+
+      ++it;
+    }
+  });
+};
+
+
+exports["removeChild: no siblings"] = function () {
+  let parent;
+  let children;
+  let it;
+
+  return suite({
+    setup: function (document) {
+      parent = [];
+      children = [];
+
+      for (let i = 0; i < this.count; ++i) {
+        parent.push(document.createElement("div"));
+        children.push(document.createElement("div"));
+        parent[i].appendChild(children[i]);
+      }
+
+      it = 0;
+    },
+    fn: function () {
+      parent[it].removeChild(children[it]);
+      ++it;
+    }
+  });
+};
+
+exports["removeChild: many siblings"] = function () {
+  let parent;
+  let children;
+  let it;
+
+  return suite({
+    setup: function (document) {
+      parent = document.createElement("div");
+      children = [];
+
+      for (let i = 0; i < this.count; ++i) {
+        children.push(document.createElement("div"));
+        parent.appendChild(children[i]);
+      }
+
+      it = 0;
+    },
+    fn: function () {
+      parent.removeChild(children[it]);
+      ++it;
+    }
+  });
+};
+
+exports["removeChild: many parents"] = function () {
+  const DEPTH = 100;
+  let itData;
+  let it;
+
+  return suite({
+    setup: function (document) {
+      itData = new Array(this.count);
+
+      for (let i = 0; i < this.count; ++i) {
+        let nodes = new Array(DEPTH + 1);
+        nodes[0] = document.createElement("div");
+
+        for (let n = 0; n < DEPTH; ++n) {
+          nodes[n + 1] = document.createElement("div");
+          nodes[n].appendChild(nodes[n + 1]);
+        }
+
+        itData[i] = nodes;
+      }
+
+      it = 0;
+    },
+    fn: function () {
+      let nodes = itData[it];
+
+      for (let n = 0; n < DEPTH; ++n) {
+        nodes[n].removeChild(nodes[n + 1]);
+      }
+
+      ++it;
+    }
+  });
+};
