acc8bfc3b993fbc3df6f9f6ee7517989a3217d3d
Robin Berjon <robin@berjon.com>
2015-09-14 10:10:59 -0400

    make NodeList iterable

--- test/living-dom/node-list.js
+++ test/living-dom/node-list.js
@@ -8,3 +8,30 @@ exports["Object.keys on a NodeList gives the correct keys"] = function (t) {
   t.deepEqual(Object.keys(nodeList), ["0", "1", "2"]);
   t.done();
 };
+
+exports["NodeList is for..of iterable"] = function (t) {
+  const document = jsdom.jsdom("<p>1</p><p>2</p><p>3</p>");
+  const nodeList = document.querySelectorAll("p");
+
+  let total = 0;
+  for (let p of nodeList) {
+    total++;
+    t.equal(p.tagName, "P");
+  }
+  t.equal(total, 3);
+  t.done();
+};
+
+exports["NodeList is for..of iterable on live content"] = function (t) {
+  const document = jsdom.jsdom("<p>1</p><p>2</p><p>3</p>");
+  const nodeList = document.body.childNodes;
+
+  let total = 0;
+  for (let p of nodeList) {
+    total++;
+    t.equal(p.tagName, "P");
+    if (total < 3) document.body.appendChild(document.createElement("p"));
+  }
+  t.equal(total, 5);
+  t.done();
+};
