2ee7fe0666ce21c8b50f2a6b2bf50257271a6ca0
zaucy <ezekiel@robotoppression.com>
2015-09-13 22:23:08 -0700

    Implemented ChildNode Interface

---
+++ test/living-dom/child-node.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var jsdom = require("../..");
+var types = require("../lib/jsdom/node-type.js");
+
+function createChildNodeTestDoc() {
+  var doc = jsdom(`
+<div id="one">1</div>
+<div id="two">2</div>
+<div id="three">3</div>
+`;
+  doc.one = doc.getElementById("one");
+  doc.two = doc.getElementById("two");
+  doc.three = doc.getElementById("three");
+
+  return doc;
+}
+
+exports["Element should implement ChildNode:before()"] = function(t) {
+  const doc = createChildNodeTestDoc();
+  doc.two.before("before");
+
+  t.strictEqual(doc.two.previousSibling.textContent, "before");
+  t.done();
+};
+
+exports["Element should implement ChildNode:after()"] = function(t) {
+  const doc = createChildNodeTestDoc();
+  doc.two.after("after");
+
+  t.strictEqual(doc.two.nextSibling.textContent, "after");
+  t.done();
+};
+
+exports["Element should implement ChildNode:replaceWith()"] = function(t) {
+  const doc = createChildNodeTestDoc();
+  const parent = doc.body;
+  doc.two.replaceWith("replace two");
+
+  t.strictEqual(parent.children[1].nodeType, types.TEXT_NODE);
+  t.strictEqual(parent.children[1].textContent, "replace two");
+  t.strictEqual(doc.two.parentNode, null);
+  t.done();
+};
+
+exports["Element should implement ChildNode:remove()"] = function(t) {
+  const doc = createChildNodeTestDoc();
+  doc.two.remove();
+  t.strictEqual(doc.two.parentNode, null);
+  t.done();
+};
