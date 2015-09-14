2782e4eda1939dea615d5b124ebf8dd559864bbc
zaucy <ezekiel@robotoppression.com>
2015-09-14 12:17:18 -0700

    More spec compliant. Fixed lint issues.

--- test/living-dom/child-node.js
+++ test/living-dom/child-node.js
@@ -1,14 +1,13 @@
 "use strict";
 
-var jsdom = require("../..");
-var types = require("../lib/jsdom/node-type.js");
+const jsdom = require("../..");
 
 function createChildNodeTestDoc() {
-  var doc = jsdom(`
+  let doc = jsdom(`
 <div id="one">1</div>
 <div id="two">2</div>
-<div id="three">3</div>
-`;
+<div id="three">3</div>`);
+
   doc.one = doc.getElementById("one");
   doc.two = doc.getElementById("two");
   doc.three = doc.getElementById("three");
@@ -16,7 +15,7 @@ function createChildNodeTestDoc() {
   return doc;
 }
 
-exports["Element should implement ChildNode:before()"] = function(t) {
+exports["Element should implement ChildNode:before()"] = function (t) {
   const doc = createChildNodeTestDoc();
   doc.two.before("before");
 
@@ -24,7 +23,7 @@ exports["Element should implement ChildNode:before()"] = function(t) {
   t.done();
 };
 
-exports["Element should implement ChildNode:after()"] = function(t) {
+exports["Element should implement ChildNode:after()"] = function (t) {
   const doc = createChildNodeTestDoc();
   doc.two.after("after");
 
@@ -32,18 +31,18 @@ exports["Element should implement ChildNode:after()"] = function(t) {
   t.done();
 };
 
-exports["Element should implement ChildNode:replaceWith()"] = function(t) {
+exports["Element should implement ChildNode:replaceWith()"] = function (t) {
   const doc = createChildNodeTestDoc();
   const parent = doc.body;
   doc.two.replaceWith("replace two");
 
-  t.strictEqual(parent.children[1].nodeType, types.TEXT_NODE);
+  t.strictEqual(parent.children[1].nodeType, doc.TEXT_NODE);
   t.strictEqual(parent.children[1].textContent, "replace two");
   t.strictEqual(doc.two.parentNode, null);
   t.done();
 };
 
-exports["Element should implement ChildNode:remove()"] = function(t) {
+exports["Element should implement ChildNode:remove()"] = function (t) {
   const doc = createChildNodeTestDoc();
   doc.two.remove();
   t.strictEqual(doc.two.parentNode, null);
