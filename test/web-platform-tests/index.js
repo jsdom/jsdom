376c48c71646af195b5911581b72aefb496324c6
Evan Jacobs <evan.jacobs@enigma.io>
2015-11-29 23:27:30 -0500

    Input selection tests + behaviorial adjustment
    Two changes to the original commit:
    
    1. Setting value should not cause a select event
    2. select() should throw if the input doesn't support it
    
    Other changes:
    
    1. Enable web platform tests for input selection
    2. Wrote several more to test various nuances

--- test/web-platform-tests/index.js
+++ test/web-platform-tests/index.js
@@ -61,6 +61,7 @@ const runWebPlatformTest = require("./run-web-platform-test")(exports, path.reso
   "html/dom/dynamic-markup-insertion/document-writeln/document.writeln-03.html",
   "html/dom/elements/global-attributes/classlist-nonstring.html",
   "html/infrastructure/urls/terminology-0/document-base-url.html",
+  "html/semantics/forms/the-input-element/input-textselection-01.html",
   // "html/semantics/scripting-1/the-template-element/additions-to-parsing-xhtml-documents/node-document.html", // templates in XHTML are totally messed up
   // "html/semantics/scripting-1/the-template-element/additions-to-parsing-xhtml-documents/template-child-nodes.html", // templates in XHTML are totally messed up
   // "html/semantics/scripting-1/the-template-element/additions-to-serializing-xhtml-documents/outerhtml.html", // templates in XHTML are totally messed up
