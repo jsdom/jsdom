jsdom/jsdom

commit 17ac5a47e0104ca8e1c77cc64b90e2094edffcca

Fix #1311: HTMLCollection would get confused by numeric IDs

lib/jsdom/living/html-collection.js
@@ -68,6 +68,13 @@ function resetHTMLCollectionTo(collection, els) {
 
   function addIfAttrPresent(el, attr) {
     const value = getAttributeValue(el, attr);
+
+    // Don't overwrite numeric indices with named ones.
+    const valueAsNumber = Number(value);
+    if (Number.isNaN(valueAsNumber) || valueAsNumber >= 0) {
+      return;
+    }
+
     if (value) {
       collection[value] = el;
       keys.push(value);

test/web-platform-tests/to-upstream/dom/nodes/getElementsByClassName-32.html
@@ -1,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<title>Node.prototype.getElementsByClassName tests imported from jsdom</title>
+<link rel=help href="https://dom.spec.whatwg.org/#dom-document-getelementsbyclassname">
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+
+<div class="df-article" id="1">
+</div>
+<div class="df-article" id="2">
+</div>
+<div class="df-article" id="3">
+</div>
+
+<script>
+"use strict";
+
+test(() => {
+
+  const p = document.createElement("p");
+  p.className = "unknown";
+  document.body.appendChild(p);
+
+  const elements = document.getElementsByClassName("first-p");
+  assert_array_equals(elements, []);
+
+}, "cannot find the class name");
+
+test(() => {
+
+  const p = document.createElement("p");
+  p.className = "first-p";
+  document.body.appendChild(p);
+
+  const elements = document.getElementsByClassName("first-p");
+  assert_array_equals(elements, [p]);
+
+}, "finds the class name");
+
+
+test(() => {
+
+  const p = document.createElement("p");
+  p.className = "the-p second third";
+  document.body.appendChild(p);
+
+  const elements1 = document.getElementsByClassName("the-p");
+  assert_array_equals(elements1, [p]);
+
+  const elements2 = document.getElementsByClassName("second");
+  assert_array_equals(elements2, [p]);
+
+  const elements3 = document.getElementsByClassName("third");
+  assert_array_equals(elements3, [p]);
+
+}, "finds the same element with multiple class names");
+
+test(() => {
+
+  const elements = document.getElementsByClassName("df-article");
+
+  assert_equals(elements.length, 3);
+  assert_array_equals(Array.prototype.map.call(elements, el => el.id), ["1", "2", "3"]);
+
+}, "does not get confused by numeric IDs")
+
+</script>

commit 1243b66cef79dc62b347ddd05b0db4fdb1d4bfc2

Fix HTMLCollection fix
17ac5a47e0104ca8e1c77cc64b90e2094edffcca introduced a bug in ID/name tracking. Oops.

lib/jsdom/living/html-collection.js
@@ -69,16 +69,18 @@ function resetHTMLCollectionTo(collection, els) {
   function addIfAttrPresent(el, attr) {
     const value = getAttributeValue(el, attr);
 
-    // Don't overwrite numeric indices with named ones.
-    const valueAsNumber = Number(value);
-    if (Number.isNaN(valueAsNumber) || valueAsNumber >= 0) {
+    if (value === null || value === "") {
       return;
     }
 
-    if (value) {
-      collection[value] = el;
-      keys.push(value);
+    // Don't overwrite numeric indices with named ones.
+    const valueAsNumber = Number(value);
+    if (!Number.isNaN(valueAsNumber) && valueAsNumber >= 0) {
+      return;
     }
+
+    collection[value] = el;
+    keys.push(value);
   }
 }
 

test/web-platform-tests/to-upstream/dom/nodes/getElementsByClassName-32.html
@@ -63,6 +63,6 @@ test(() => {
   assert_equals(elements.length, 3);
   assert_array_equals(Array.prototype.map.call(elements, el => el.id), ["1", "2", "3"]);
 
-}, "does not get confused by numeric IDs")
+}, "does not get confused by numeric IDs");
 
 </script>