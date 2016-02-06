6bd5ee871003ef84e31d3499b2b10193769d26a8
Michael Frye <michael@michaelfrye.me>
2016-02-05 18:52:23 -0800

    Fix style errors

--- test/web-platform-tests/to-upstream/browsers/windows/nested-browsing-contexts/frame.js
+++ test/web-platform-tests/to-upstream/browsers/windows/nested-browsing-contexts/frame.js
@@ -1,24 +1,24 @@
-var path = require('path');
-var fs = require('fs');
+var path = require("path");
+var fs = require("fs");
 
-var root = '../../../../..';
+var root = "../../../../..";
 var jsdom = require(root);
-var toFileUrl = require(path.join(root, 'test/util')).toFileUrl(__dirname);
+var toFileUrl = require(path.join(root, "test/util")).toFileUrl(__dirname);
 
 exports.tests = {
 
-  'iframe document referrer' : function(test) {
-    var htmlPath = path.resolve(__dirname, 'files', 'iframe_parent.html');
-    var doc = jsdom.jsdom(fs.readFileSync(htmlPath, 'utf8'), {
+  "iframe document referrer" : function(test) {
+    var htmlPath = path.resolve(__dirname, "files", "iframe_parent.html");
+    var doc = jsdom.jsdom(fs.readFileSync(htmlPath, "utf8"), {
       features : {
-        FetchExternalResources   : ['script', 'iframe'],
-        ProcessExternalResources : ['script', 'iframe']
+        FetchExternalResources   : ["script", "iframe"],
+        ProcessExternalResources : ["script", "iframe"]
       },
       url : toFileUrl(__filename)
     });
 
-    doc.addEventListener('load', function () {
-      var iframeElem = doc.getElementById('simpleIFrameID');
+    doc.addEventListener("load", function () {
+      var iframeElem = doc.getElementById("simpleIFrameID");
       test.notEqual(iframeElem, null);
       var iframeDoc = iframeElem.contentDocument;
       test.notEqual(iframeDoc, null);
