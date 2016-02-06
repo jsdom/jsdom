ff7d1edfa469658043155886253b04462d9dd088
Michael Frye <michael@michaelfrye.me>
2016-02-05 18:24:03 -0800

    document referrer test

---
+++ test/web-platform-tests/to-upstream/browsers/windows/nested-browsing-contexts/frame.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var fs = require('fs');
+
+var root = '../../../../..';
+var jsdom = require(root);
+var toFileUrl = require(path.join(root, 'test/util')).toFileUrl(__dirname);
+
+exports.tests = {
+
+  'iframe document referrer' : function(test) {
+    var htmlPath = path.resolve(__dirname, 'files', 'iframe_parent.html');
+    var doc = jsdom.jsdom(fs.readFileSync(htmlPath, 'utf8'), {
+      features : {
+        FetchExternalResources   : ['script', 'iframe'],
+        ProcessExternalResources : ['script', 'iframe']
+      },
+      url : toFileUrl(__filename)
+    });
+
+    doc.addEventListener('load', function () {
+      var iframeElem = doc.getElementById('simpleIFrameID');
+      test.notEqual(iframeElem, null);
+      var iframeDoc = iframeElem.contentDocument;
+      test.notEqual(iframeDoc, null);
+      test.equal(iframeDoc.referrer, toFileUrl(__filename));
+      test.done();
+    });
+  },
+};
