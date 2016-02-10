b6fbc2c9f3605ec4791579f5ac2c5a51ac9771d8
Joris van der Wel <joris@jorisvanderwel.com>
2016-02-10 23:52:06 +0100

    Fix test/util.js code style

--- test/util.js
+++ test/util.js
@@ -1,48 +1,48 @@
 "use strict";
-var path = require('path');
-var jsdom = require('../lib/jsdom');
-var fs = require('fs');
+const path = require("path");
+const jsdom = require("../lib/jsdom");
+const fs = require("fs");
 const exceptionTable = require("../lib/jsdom/web-idl/dom-exception-table.json");
 
 function toPathname(dirname, relativePath) {
-  var pathname = path.resolve(dirname, relativePath).replace(/\\/g, '/');
-  if (pathname[0] !== '/') {
-    pathname = '/' + pathname;
+  let pathname = path.resolve(dirname, relativePath).replace(/\\/g, "/");
+  if (pathname[0] !== "/") {
+    pathname = "/" + pathname;
   }
   return pathname;
 }
 
 function toFileUrl(dirname, relativePath) {
-  return 'file://' + toPathname(dirname, relativePath);
+  return "file://" + toPathname(dirname, relativePath);
 }
 
-exports.toFileUrl = function (dirname) {
+exports.toFileUrl = dirname => {
   return function (relativePath) {
     return toFileUrl(dirname, relativePath);
   };
 };
 
-exports.toPathname = function (dirname) {
+exports.toPathname = dirname => {
   return function (relativePath) {
     return toPathname(dirname, relativePath);
   };
 };
 
-exports.load = function (dirname) {
-  var fileCache = Object.create(null);
+exports.load = dirname => {
+  const fileCache = Object.create(null);
 
   return function (name, options) {
     options = options || {};
 
-    var file = path.resolve(dirname, 'files/' + name + '.html');
+    const file = path.resolve(dirname, "files/" + name + ".html");
 
     if (!options.url) {
       options.url = toFileUrl(dirname, file);
     }
 
-    var contents = fileCache[file] || fs.readFileSync(file, 'utf8');
-    var doc = jsdom.jsdom(null, options);
-    var window = doc.defaultView;
+    const contents = fileCache[file] || fs.readFileSync(file, "utf8");
+    const doc = jsdom.jsdom(null, options);
+    const window = doc.defaultView;
 
     doc.parent = window;
     window.loadComplete = function () {};
@@ -65,7 +65,7 @@ exports.load = function (dirname) {
  *   // ...
  * }, domExceptionPredicate(doc, "NoModificationAllowedError"));
  */
-exports.domExceptionPredicate = function (document, name) {
+exports.domExceptionPredicate = (document, name) => {
   return function (error) {
     return error instanceof document.defaultView.DOMException &&
            error.name === name &&
@@ -73,20 +73,20 @@ exports.domExceptionPredicate = function (document, name) {
   };
 };
 
-exports.todo = function (test, fn) {
+exports.todo = (test, fn) => {
   fn({
-    ok: function (value, message) {
+    ok(value, message) {
       test.ok(!value, "Marked as TODO: " + message);
     }
     // add more as needed
   });
 };
 
-exports.injectIFrame = function (document) {
+exports.injectIFrame = document => {
   return exports.injectIFrameWithScript(document);
 };
 
-exports.injectIFrameWithScript = function (document, scriptStr) {
+exports.injectIFrameWithScript = (document, scriptStr) => {
   scriptStr = scriptStr || "";
   const iframe = document.createElement("iframe");
   document.body.appendChild(iframe);
