c1aa53150d18d0a80703e107ee5615872f81088c
Sebastian Mayr <github@smayr.name>
2016-05-28 13:29:23 -0400

    Update idl conversion script to new webidl2js

--- scripts/webidl/convert.js
+++ scripts/webidl/convert.js
@@ -1,56 +1,23 @@
 "use strict";
-const fs = require("fs");
+
 const path = require("path");
-const Q = require("q");
-const readdirRecursive = require("fs-readdir-recursive");
-const webidl2js = require("webidl2js");
 
-const outputDir = path.resolve(__dirname, "../../lib/jsdom/living/generated/");
-Q.longStackSupport = true;
+const Webidl2js = require("webidl2js");
 
-function readConcatenatedSource(files) {
-  return Q.all(files.map(f => {
-    return Q.nfcall(fs.readFile, f, { encoding: "utf8" });
-  })).then(sources => {
-    let src = "";
-    for (let i = 0; i < sources.length; ++i) {
-      src += sources[i];
-    }
-    return src;
-  });
+const transformer = new Webidl2js();
+transformer.options.suppressErrors = true;
+transformer.options.implSuffix = "-impl";
+
+function addDir(dir) {
+  const resolved = path.resolve(__dirname, dir);
+  transformer.addSource(resolved, resolved);
 }
 
-function generateClasses(src, implDir) {
-  webidl2js.generate(src, outputDir, implDir, { suppressErrors: true, implSuffix: "-impl" });
-}
+addDir(path.resolve(__dirname, "../../lib/jsdom/living/traversal"));
+addDir(path.resolve(__dirname, "../../lib/jsdom/living/events"));
+addDir(path.resolve(__dirname, "../../lib/jsdom/living/attributes"));
+addDir(path.resolve(__dirname, "../../lib/jsdom/living/window"));
+addDir(path.resolve(__dirname, "../../lib/jsdom/living/nodes"));
+addDir(path.resolve(__dirname, "../../lib/jsdom/living/navigator"));
 
-function doConversion(inputPath) {
-  let isDir;
-
-  return Q.nfcall(fs.stat, inputPath)
-  .then(inputStat => {
-    isDir = inputStat.isDirectory();
-    if (isDir) {
-      return readdirRecursive(inputPath, onlyIDL).map(relativePath => path.resolve(inputPath, relativePath));
-    }
-
-    return [inputPath]; // get dir name
-  })
-  .then(readConcatenatedSource)
-  .then(src => {
-    const folder = isDir ? inputPath : path.dirname(inputPath);
-    generateClasses(src, folder);
-  });
-}
-
-function onlyIDL(filePath) {
-  return path.extname(filePath) === ".idl";
-}
-
-doConversion(path.resolve(__dirname, "../../lib/jsdom/living/traversal"))
-  .then(() => doConversion(path.resolve(__dirname, "../../lib/jsdom/living/events")))
-  .then(() => doConversion(path.resolve(__dirname, "../../lib/jsdom/living/attributes")))
-  .then(() => doConversion(path.resolve(__dirname, "../../lib/jsdom/living/window")))
-  .then(() => doConversion(path.resolve(__dirname, "../../lib/jsdom/living/nodes")))
-  .then(() => doConversion(path.resolve(__dirname, "../../lib/jsdom/living/navigator")))
-  .done();
+transformer.generate(path.resolve(__dirname, "../../lib/jsdom/living/generated/")).done();
