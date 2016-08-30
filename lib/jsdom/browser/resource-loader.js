jsdom/jsdom

commit f7de36384fe10a0ba5969d678d8ffe7187257bdc

Handle aborting resource loads during shutdown better

lib/jsdom/browser/resource-loader.js
@@ -35,17 +35,21 @@ function createResourceLoadHandler(element, resourceUrl, document, loadCallback)
     }
 
     if (err) {
-      ev.initEvent("error", false, false);
-      ev.error = err;
+      if (!err.isAbortError) {
+        ev.initEvent("error", false, false);
+        ev.error = err;
 
-      const error = new Error(`Could not load ${element.localName}: "${resourceUrl}"`);
-      error.detail = err;
-      error.type = "resource loading";
+        console.log("what error did we get?", err);
 
-      document._defaultView._virtualConsole.emit("jsdomError", error);
+        const error = new Error(`Could not load ${element.localName}: "${resourceUrl}"`);
+        error.detail = err;
+        error.type = "resource loading";
+
+        document._defaultView._virtualConsole.emit("jsdomError", error);
+      }
+    } else {
+      element.dispatchEvent(ev);
     }
-
-    element.dispatchEvent(ev);
   };
 }
 
@@ -79,7 +83,9 @@ exports.readFile = function (filePath, options, callback) {
   return {
     abort() {
       readableStream.destroy();
-      callback(new Error("request canceled by user"));
+      const error = new Error("request canceled by user");
+      error.isAbortError = true;
+      callback(error);
     }
   };
 };
@@ -192,7 +198,9 @@ exports.download = function (url, options, callback) {
   return {
     abort() {
       req.abort();
-      callback(new Error("request canceled by user"));
+      const error = new Error("request canceled by user");
+      error.isAbortError = true;
+      callback(error);
     }
   };
 };

commit b8ef33b7f639d47d2ffb20f1b25c05fd7c190525

Dispatch non-abort errors when loading
Another regression from f7de36384fe10a0ba5969d678d8ffe7187257bdc

lib/jsdom/browser/resource-loader.js
@@ -38,6 +38,7 @@ function createResourceLoadHandler(element, resourceUrl, document, loadCallback)
       if (!err.isAbortError) {
         ev.initEvent("error", false, false);
         ev.error = err;
+        element.dispatchEvent(ev);
 
         const error = new Error(`Could not load ${element.localName}: "${resourceUrl}"`);
         error.detail = err;