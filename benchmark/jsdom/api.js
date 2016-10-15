jsdom/jsdom

commit a7cd5a160fd6c295fa80e953fdf051a1c5dc5cf5

Further fixes in light of HTMLCollection changes
Notably, we were not correctly updating the version of a node if its children or ancestors changed! This caused our invalidation logic to be very off, unless we used the document as the versioning root (which, previously, we often did).

Some of the older tests were wrong; replaced with to-upstream versions with corrected semantics.

lib/jsdom/living/nodes/Node-impl.js
@@ -229,7 +229,13 @@ class NodeImpl extends EventTargetImpl {
   } // raises(DOMException);
 
   _modified() {
-    this._version++;
+    for (const inclusiveDescendant of domSymbolTree.treeIterator(this)) {
+      inclusiveDescendant._version++;
+    }
+    for (const ancestor of domSymbolTree.ancestorsIterator(this)) {
+      ancestor._version++;
+    }
+
     if (this._ownerDocument) {
       this._ownerDocument._version++;
     }

lib/jsdom/living/helpers/traversal.js
@@ -32,6 +32,14 @@ exports.childrenByHTMLLocalNames = (parent, localNamesSet) => {
   } });
 };
 
+exports.descendantsByHTMLLocalNames = (parent, localNamesSet) => {
+  return domSymbolTree.treeToArray(parent, { filter(node) {
+    return localNamesSet.has(node._localName) &&
+           node._namespaceURI === "http://www.w3.org/1999/xhtml" &&
+           node !== parent;
+  } });
+};
+
 exports.firstChildWithHTMLLocalName = (parent, localName) => {
   const iterator = domSymbolTree.childrenIterator(parent);
   for (const child of iterator) {

lib/jsdom/living/nodes/HTMLFormElement-impl.js
@@ -2,7 +2,7 @@
 
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
 const idlUtils = require("../generated/utils");
-const childrenByHTMLLocalNames = require("../helpers/traversal").childrenByHTMLLocalNames;
+const descendantsByHTMLLocalNames = require("../helpers/traversal").descendantsByHTMLLocalNames;
 const domSymbolTree = require("../helpers/internal-constants").domSymbolTree;
 const createHTMLCollection = require("../../living/html-collection").create;
 const notImplemented = require("../../browser/not-implemented");
@@ -46,7 +46,7 @@ class HTMLFormElementImpl extends HTMLElementImpl {
   }
 
   get elements() {
-    return createHTMLCollection(this._ownerDocument, () => childrenByHTMLLocalNames(this, listedElements));
+    return createHTMLCollection(this, () => descendantsByHTMLLocalNames(this, listedElements));
   }
 
   get length() {

lib/jsdom/living/nodes/HTMLTableElement-impl.js
@@ -4,7 +4,6 @@ const HTMLElementImpl = require("./HTMLElement-impl").implementation;
 
 const firstChildWithHTMLLocalName = require("../helpers/traversal").firstChildWithHTMLLocalName;
 const childrenByHTMLLocalName = require("../helpers/traversal").childrenByHTMLLocalName;
-const domSymbolTree = require("../helpers/internal-constants").domSymbolTree;
 const createHTMLCollection = require("../../living/html-collection").create;
 const DOMException = require("../../web-idl/DOMException");
 const idlUtils = require("../generated/utils");
@@ -67,7 +66,7 @@ class HTMLTableElementImpl extends HTMLElementImpl {
   deleteTHead() {
     const el = this.tHead;
     if (el) {
-      domSymbolTree.parent(el).removeChild(el);
+      el.parentNode.removeChild(el);
     }
   }
 
@@ -83,7 +82,7 @@ class HTMLTableElementImpl extends HTMLElementImpl {
   deleteTFoot() {
     const el = this.tFoot;
     if (el) {
-      domSymbolTree.parent(el).removeChild(el);
+      el.parentNode.removeChild(el);
     }
   }
 
@@ -99,43 +98,48 @@ class HTMLTableElementImpl extends HTMLElementImpl {
   deleteCaption() {
     const c = this.caption;
     if (c) {
-      domSymbolTree.parent(c).removeChild(c);
+      c.parentNode.removeChild(c);
     }
   }
 
   insertRow(index) {
-    const tr = this._ownerDocument.createElement("TR");
+    if (index < -1 || index > this.rows.length) {
+      throw new DOMException(DOMException.INDEX_SIZE_ERR,
+        "Cannot insert a row at an index that is less than -1 or greater than the number of existing rows");
+    }
 
-    if (!domSymbolTree.hasChildren(this)) {
-      this.appendChild(this._ownerDocument.createElement("TBODY"));
-    }
-    const rows = this.rows;
-    if (index < -1 || index > rows.length) {
-      throw new DOMException(DOMException.INDEX_SIZE_ERR);
-    }
-    if (index === -1 || (index === 0 && rows.length === 0)) {
-      this.tBodies.item(0).appendChild(tr);
-    } else if (index === rows.length) {
-      const ref = idlUtils.implForWrapper(rows[index - 1]);
-      domSymbolTree.parent(ref).appendChild(tr);
+    const tr = this._ownerDocument.createElement("tr");
+
+    if (this.rows.length === 0 && this.tBodies.length === 0) {
+      const tBody = this._ownerDocument.createElement("tbody");
+      tBody.appendChild(tr);
+      this.appendChild(tBody);
+    } else if (this.rows.length === 0) {
+      const tBody = idlUtils.implForWrapper(this.tBodies[this.tBodies.length - 1]);
+      tBody.appendChild(tr);
+    } else if (index === -1 || index === this.rows.length) {
+      const tSection = idlUtils.implForWrapper(this.rows[this.rows.length - 1]).parentNode;
+      tSection.appendChild(tr);
     } else {
-      const ref = idlUtils.implForWrapper(rows[index]);
-      domSymbolTree.parent(ref).insertBefore(tr, ref);
+      const beforeTR = idlUtils.implForWrapper(this.rows[index]);
+      const tSection = beforeTR.parentNode;
+      tSection.insertBefore(tr, beforeTR);
     }
+
     return tr;
   }
 
   deleteRow(index) {
-    const rows = this.rows;
-    const l = rows.length;
     if (index === -1) {
-      index = l - 1;
+      index = this.rows.length - 1;
     }
-    if (index < 0 || index >= l) {
-      throw new DOMException(DOMException.INDEX_SIZE_ERR);
+
+    if (index < 0 || index >= this.rows.length) {
+      throw new DOMException(DOMException.INDEX_SIZE_ERR, `Cannot delete a row at index ${index}, where no row exists`);
     }
-    const tr = idlUtils.implForWrapper(rows[index]);
-    domSymbolTree.parent(tr).removeChild(tr);
+
+    const tr = idlUtils.implForWrapper(this.rows[index]);
+    tr.parentNode.removeChild(tr);
   }
 }
 

lib/jsdom/living/nodes/HTMLTableSectionElement-impl.js
@@ -15,30 +15,37 @@ class HTMLTableSectionElementImpl extends HTMLElementImpl {
   }
 
   insertRow(index) {
-    const tr = this._ownerDocument.createElement("TR");
-    const rows = this.rows;
-    if (index < -1 || index > rows.length) {
-      throw new DOMException(DOMException.INDEX_SIZE_ERR);
+    if (index < -1 || index > this.rows.length) {
+      throw new DOMException(DOMException.INDEX_SIZE_ERR,
+        "Cannot insert a row at an index that is less than -1 or greater than the number of existing rows");
     }
-    if (index === -1 || index === rows.length) {
+
+    const tr = this._ownerDocument.createElement("tr");
+
+    if (index === -1 || index === this.rows.length) {
       this.appendChild(tr);
     } else {
-      const ref = idlUtils.implForWrapper(rows[index]);
-      this.insertBefore(tr, ref);
+      const beforeTR = idlUtils.implForWrapper(this.rows[index]);
+      this.insertBefore(tr, beforeTR);
     }
+
     return tr;
   }
 
   deleteRow(index) {
-    const rows = this.rows;
+    if (index < -1 || index >= this.rows.length) {
+      throw new DOMException(DOMException.INDEX_SIZE_ERR, `Cannot delete a row at index ${index}, where no row exists`);
+    }
+
     if (index === -1) {
-      index = rows.length - 1;
+      if (this.rows.length > 0) {
+        const tr = idlUtils.implForWrapper(this.rows[this.rows.length - 1]);
+        this.removeChild(tr);
+      }
+    } else {
+      const tr = idlUtils.implForWrapper(this.rows[index]);
+      this.removeChild(tr);
     }
-    if (index < 0 || index >= rows.length) {
-      throw new DOMException(DOMException.INDEX_SIZE_ERR);
-    }
-    const tr = idlUtils.implForWrapper(rows[index]);
-    this.removeChild(tr);
   }
 }
 

test/level2/html.js
@@ -2227,33 +2227,6 @@ exports.tests = {
 
   /**
    *
-   The applets attribute returns a collection of all OBJECT elements that
-   include applets abd APPLET elements in a document.
-   Retrieve the applets attribute from the document and examine its value.
-   * @author NIST
-   * @author Rick Rivello
-   * @see http://www.w3.org/TR/1998/REC-DOM-Level-1-19981001/level-one-html#ID-85113862
-   */
-  HTMLDocument08: function(test) {
-    var success;
-    var nodeList;
-    var testNode;
-    var vapplets;
-    var vlength;
-    var doc;
-    var docRef = null;
-    if (typeof(this.doc) != 'undefined') {
-      docRef = this.doc;
-    }
-    doc = load("document");
-    vapplets = doc.applets;
-    vlength = vapplets.length;
-    test.equal(vlength, 4, "length");
-    test.done();
-  },
-
-  /**
-   *
    The links attribute returns a collection of all AREA and A elements
    in a document with a value for the href attribute.
    Retrieve the links attribute from the document and examine its value.
@@ -14538,54 +14511,6 @@ exports.tests = {
       }
       test.ok(success, 'HTMLTableElement38');
     }
-    test.done();
-  },
-
-  /**
-   *
-   The insertRow() method inserts a new empty table row.
-   If index is -1 or equal to the number of rows, the new row
-   is appended.
-   Retrieve the second TABLE element and invoke the insertRow() method
-   with an index of negative one.
-   The number of rows in the TBODY section before insertion with an index
-   of negative one is two.  After the new row is inserted the number
-   of rows in the TBODY section is three.
-   * @author NIST
-   * @author Rick Rivello
-   * @see http://www.w3.org/TR/DOM-Level-2-HTML/html#ID-39872903
-   */
-  HTMLTableElement39: function(test) {
-    var success;
-    var nodeList;
-    var tbodiesnodeList;
-    var testNode;
-    var bodyNode;
-    var newRow;
-    var rowsnodeList;
-    var vsection1;
-    var vsection2;
-    var vrows;
-    var doc;
-    var docRef = null;
-    if (typeof(this.doc) != 'undefined') {
-      docRef = this.doc;
-    }
-    doc = load("table");
-    nodeList = doc.getElementsByTagName("table");
-    test.equal(nodeList.length, 3, 'Asize');
-    testNode = nodeList.item(1);
-    tbodiesnodeList = testNode.tBodies;
-    bodyNode = tbodiesnodeList.item(0);
-    rowsnodeList = bodyNode.rows;
-    vrows = rowsnodeList.length;
-    test.equal(vrows, 2, "rowsLink1");
-    newRow = testNode.insertRow(-1);
-    tbodiesnodeList = testNode.tBodies;
-    bodyNode = tbodiesnodeList.item(0);
-    rowsnodeList = bodyNode.rows;
-    vrows = rowsnodeList.length;
-    test.equal(vrows, 3, "rowsLink2");
     test.done();
   },
 

test/web-platform-tests/index.js
@@ -152,8 +152,8 @@ describe("Web Platform Tests", () => {
     "html/semantics/tabular-data/the-table-element/delete-caption.html",
     "html/semantics/tabular-data/the-table-element/insertRow-method-01.html",
     "html/semantics/tabular-data/the-table-element/insertRow-method-02.html",
-    // "html/semantics/tabular-data/the-table-element/tBodies.html",
-    // "html/semantics/tabular-data/the-table-element/table-insertRow.html",
+    "html/semantics/tabular-data/the-table-element/tBodies.html",
+    "html/semantics/tabular-data/the-table-element/table-insertRow.html",
     // "html/semantics/tabular-data/the-table-element/table-rows.html",
     "html/syntax/serializing-html-fragments/outerHTML.html",
     // "html/syntax/parsing/html5lib_template.html", // no idea what's going on here

test/web-platform-tests/to-upstream.js
@@ -43,6 +43,7 @@ describe("Local tests in Web Platform Test format (to-upstream)", () => {
     "html/named-access-on-window/nested-context.html",
     "html/named-access-on-window/only-name.html",
     "html/named-access-on-window/removing.html",
+    "html/obsolete/requirements-for-implementations/other-elements-attributes-and-apis/applets.html",
     "html/semantics/document-metadata/the-link-element/stylesheet-appropriate-time-to-obtain.html",
     "html/semantics/forms/resetting-a-form/reset-form-2.html",
     "html/semantics/forms/the-button-element/button-click-submits.html",
@@ -62,6 +63,7 @@ describe("Local tests in Web Platform Test format (to-upstream)", () => {
     "html/semantics/links/links-created-by-a-and-area-elements/html-hyperlink-element-utils-href.html",
     "html/semantics/scripting-1/the-script-element/script-languages-dont-upstream.html",
     "html/semantics/scripting-1/the-script-element/changing-src.html",
+    "html/semantics/tabular-data/the-table-element/insertRow-method-03.html",
     "html/semantics/tabular-data/the-table-element/parentless-props.html",
     "html/webappapis/events/event-handler-processing-algorithm-non-booleans.html",
     "html/webappapis/timers/arguments.html",

commit 1004684e2fb7a46dccd4e55c6a6a5d38007605b4

Fix performance regression when modifying the DOM
The _version field is used for invalidating HTMLCollections and live NodeLists. It is not necessary to increment it for descendants when a node is modified, as the filters used in HTMLCollections and NodeLists do not ever look at siblings, only at descendants, so modifying a node only needs to increment its version and its ancestors' version.

This regression was introduced in https://github.com/tmpvar/jsdom/commit/a7cd5a160fd6c295fa80e953fdf051a1c5dc5cf5, i.e. between v9.4.5 and v9.5.0. It  was particularly egregious during initial parse, which "modifies" the DOM every time it adds a node. Fixes #1625; see that issue for more details.

benchmark/jsdom/api.js
@@ -47,3 +47,21 @@ exports["jsdom() no resources"] = function () {
 
   return suite;
 };
+
+exports["jsdom() with many elements"] = function () {
+  let html = `<!doctype html><html><head><meta charset="UTF-8"><title></title></head><body>\n\n`;
+  for (let i = 0; i < 1000; ++i) {
+    html += `<a href='${i}.html'>${i}</a>\n`;
+  }
+  html += `\n</body></html>\n`;
+
+  const suite = new Benchmark.Suite();
+
+  suite.push(jsdomBenchmark({
+    fn() {
+      jsdom.jsdom(html);
+    }
+  }));
+
+  return suite;
+};

lib/jsdom/living/nodes/Node-impl.js
@@ -229,15 +229,9 @@ class NodeImpl extends EventTargetImpl {
   } // raises(DOMException);
 
   _modified() {
-    for (const inclusiveDescendant of domSymbolTree.treeIterator(this)) {
-      inclusiveDescendant._version++;
-    }
+    this._version++;
     for (const ancestor of domSymbolTree.ancestorsIterator(this)) {
       ancestor._version++;
-    }
-
-    if (this._ownerDocument) {
-      this._ownerDocument._version++;
     }
 
     if (this._childrenList) {