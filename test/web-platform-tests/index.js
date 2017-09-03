4fb01572b5d82dfe4a5ad56fbfd21bae9c7777df
Sebastian Mayr <github@smayr.name>
2017-09-03 23:27:58 +0200

    Add missing insertRow test

--- test/web-platform-tests/index.js
+++ test/web-platform-tests/index.js
@@ -250,6 +250,7 @@ describe("Web Platform Tests", () => {
     "html/semantics/tabular-data/the-table-element/delete-caption.html",
     "html/semantics/tabular-data/the-table-element/insertRow-method-01.html",
     "html/semantics/tabular-data/the-table-element/insertRow-method-02.html",
+    "html/semantics/tabular-data/the-table-element/insertRow-method-03.html",
     "html/semantics/tabular-data/the-table-element/tBodies.html",
     "html/semantics/tabular-data/the-table-element/tHead.html",
     "html/semantics/tabular-data/the-table-element/tFoot.html",
