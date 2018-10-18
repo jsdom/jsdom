7ae571f7b15b81118146c722a2b5e8a5c3d340d0
Pierre-Marie Dartus <p.dartus@salesforce.com>
2018-10-17 17:19:34 -0700

    scaffold mutation observer

--- scripts/webidl/convert.js
+++ scripts/webidl/convert.js
@@ -18,21 +18,22 @@ function addDir(dir) {
   transformer.addSource(resolved, resolved);
 }
 
-addDir("../../lib/jsdom/living/traversal");
-addDir("../../lib/jsdom/living/events");
-addDir("../../lib/jsdom/living/attributes");
-addDir("../../lib/jsdom/living/window");
-addDir("../../lib/jsdom/living/nodes");
-addDir("../../lib/jsdom/living/navigator");
-addDir("../../lib/jsdom/living/file-api");
-addDir("../../lib/jsdom/living/xhr");
-addDir("../../lib/jsdom/living/domparsing");
-addDir("../../lib/jsdom/living/svg");
 addDir("../../lib/jsdom/living/aborting");
-addDir("../../lib/jsdom/living/websockets");
-addDir("../../lib/jsdom/living/hr-time");
+addDir("../../lib/jsdom/living/attributes");
 addDir("../../lib/jsdom/living/constraint-validation");
+addDir("../../lib/jsdom/living/domparsing");
+addDir("../../lib/jsdom/living/events");
+addDir("../../lib/jsdom/living/file-api");
+addDir("../../lib/jsdom/living/hr-time");
+addDir("../../lib/jsdom/living/mutation-observer");
+addDir("../../lib/jsdom/living/navigator");
+addDir("../../lib/jsdom/living/nodes");
+addDir("../../lib/jsdom/living/svg");
+addDir("../../lib/jsdom/living/traversal");
+addDir("../../lib/jsdom/living/websockets");
 addDir("../../lib/jsdom/living/webstorage");
+addDir("../../lib/jsdom/living/window");
+addDir("../../lib/jsdom/living/xhr");
 
 const outputDir = path.resolve(__dirname, "../../lib/jsdom/living/generated/");
 
