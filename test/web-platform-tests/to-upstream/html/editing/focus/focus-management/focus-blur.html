jsdom/jsdom

commit d33cf37a4b289b14c7535487cd8be00068a3e86f

Add helpers for creating and firing events
Fixes https://github.com/jsdom/jsdom/issues/2179.

lib/jsdom/living/helpers/focusing.js
@@ -4,6 +4,7 @@ const FocusEvent = require("../generated/FocusEvent.js");
 const idlUtils = require("../generated/utils.js");
 const { isDisabled } = require("./form-controls.js");
 const { firstChildWithLocalName } = require("./traversal");
+const { fireAnEvent } = require("./events");
 const { HTML_NS } = require("./namespaces");
 
 const focusableFormElements = new Set(["input", "select", "textarea", "button"]);
@@ -60,27 +61,14 @@ exports.fireFocusEventWithTargetAdjustment = (name, target, relatedTarget) => {
     return;
   }
 
-  const event = FocusEvent.createImpl(
-    [
-      name,
-      {
-        bubbles: false,
-        cancelable: false,
-        composed: true,
-        relatedTarget,
-        view: target._ownerDocument._defaultView,
-        detail: 0
-      }
-    ],
-    {
-      isTrusted: true
-    }
-  );
-
   if (target._defaultView) {
     target = idlUtils.implForWrapper(target._defaultView);
   }
 
-  // _dispatch allows setting isTrusted
-  target._dispatch(event);
+  fireAnEvent(name, target, FocusEvent, {
+    composed: true,
+    relatedTarget,
+    view: target._ownerDocument._defaultView,
+    detail: 0
+  });
 };

lib/jsdom/browser/Window.js
@@ -28,6 +28,7 @@ const createAbortController = require("../living/generated/AbortController").cre
 const createAbortSignal = require("../living/generated/AbortSignal").createInterface;
 const reportException = require("../living/helpers/runtime-script-errors");
 const { matchesDontThrow } = require("../living/helpers/selectors");
+const { fireAnEvent } = require("../living/helpers/events");
 const SessionHistory = require("../living/window/SessionHistory");
 
 const GlobalEventHandlersImpl = require("../living/nodes/GlobalEventHandlers-impl").implementation;
@@ -631,14 +632,10 @@ function Window(options) {
     }
 
     if (window.document.readyState === "complete") {
-      const ev = window.document.createEvent("HTMLEvents");
-      ev.initEvent("load", false, false);
-      window.dispatchEvent(ev);
+      fireAnEvent("load", window);
     } else {
       window.document.addEventListener("load", () => {
-        const ev = window.document.createEvent("HTMLEvents");
-        ev.initEvent("load", false, false);
-        window.dispatchEvent(ev);
+        fireAnEvent("load", window);
       });
     }
   });

lib/jsdom/browser/resources/per-document-resource-loader.js
@@ -1,5 +1,6 @@
 "use strict";
 const idlUtils = require("../../living/generated/utils");
+const { fireAnEvent } = require("../../living/helpers/events");
 
 module.exports = class PerDocumentResourceLoader {
   constructor(document) {
@@ -25,8 +26,6 @@ module.exports = class PerDocumentResourceLoader {
 
     this._requestManager.add(request);
 
-    const ev = this._document.createEvent("HTMLEvents");
-
     const onErrorWrapped = error => {
       this._requestManager.remove(request);
 
@@ -34,9 +33,7 @@ module.exports = class PerDocumentResourceLoader {
         onError(error);
       }
 
-      // TODO: Create events in the modern way https://github.com/jsdom/jsdom/pull/2279#discussion_r199969734
-      ev.initEvent("error", false, false);
-      element.dispatchEvent(ev);
+      fireAnEvent("error", element);
 
       const err = new Error(`Could not load ${element.localName}: "${url}"`);
       err.type = "resource loading";
@@ -57,9 +54,7 @@ module.exports = class PerDocumentResourceLoader {
 
         return Promise.resolve(result)
           .then(() => {
-            // TODO: Create events in the modern way https://github.com/jsdom/jsdom/pull/2279#discussion_r199969734
-            ev.initEvent("load", false, false);
-            element.dispatchEvent(ev);
+            fireAnEvent("load", element);
 
             return Promise.resolve();
           })

lib/jsdom/living/aborting/AbortSignal-impl.js
@@ -1,9 +1,8 @@
 "use strict";
 
 const { setupForSimpleEventAccessors } = require("../helpers/create-event-accessor");
+const { fireAnEvent } = require("../helpers/events");
 const EventTargetImpl = require("../events/EventTarget-impl").implementation;
-
-const Event = require("../generated/Event");
 
 class AbortSignalImpl extends EventTargetImpl {
   constructor(args, privateData) {
@@ -27,13 +26,7 @@ class AbortSignalImpl extends EventTargetImpl {
     }
     this.abortAlgorithms.clear();
 
-    this._dispatch(Event.createImpl(
-      [
-        "abort",
-        { bubbles: false, cancelable: false }
-      ],
-      { isTrusted: true }
-    ));
+    fireAnEvent("abort", this);
   }
 
   _addAlgorithm(algorithm) {

lib/jsdom/living/constraint-validation/DefaultConstraintValidation-impl.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const ValidityState = require("../generated/ValidityState");
-const Event = require("../generated/Event");
 const { isDisabled } = require("../helpers/form-controls");
 const { closest } = require("../helpers/traversal");
+const { fireAnEvent } = require("../helpers/events");
 
 exports.implementation = class DefaultConstraintValidationImpl {
   // https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#dom-cva-willvalidate
@@ -26,7 +26,7 @@ exports.implementation = class DefaultConstraintValidationImpl {
     if (this._satisfiesConstraints()) {
       return true;
     }
-    this.dispatchEvent(Event.createImpl(["invalid", { cancelable: true }]));
+    fireAnEvent("invalid", this, undefined, { cancelable: true });
     return false;
   }
 

lib/jsdom/living/file-api/FileReader-impl.js
@@ -7,6 +7,7 @@ const DOMException = require("domexception");
 const EventTargetImpl = require("../events/EventTarget-impl").implementation;
 const ProgressEvent = require("../generated/ProgressEvent");
 const { setupForSimpleEventAccessors } = require("../helpers/create-event-accessor");
+const { fireAnEvent } = require("../helpers/events");
 
 const READY_STATES = Object.freeze({
   EMPTY: 0,
@@ -58,8 +59,7 @@ class FileReaderImpl extends EventTargetImpl {
   }
 
   _fireProgressEvent(name, props) {
-    const event = ProgressEvent.createImpl([name, Object.assign({ bubbles: false, cancelable: false }, props)], {});
-    this.dispatchEvent(event);
+    fireAnEvent(name, this, ProgressEvent, props);
   }
 
   _readFile(file, format, encoding) {

lib/jsdom/living/helpers/events.js
@@ -1,0 +1,23 @@
+"use strict";
+
+const Event = require("../generated/Event");
+const { tryImplForWrapper } = require("../generated/utils");
+
+function createAnEvent(e, eventInterface = Event, attributes = {}) {
+  return eventInterface.createImpl(
+    [e, attributes],
+    { isTrusted: attributes.isTrusted !== false }
+  );
+}
+
+function fireAnEvent(e, target, eventInterface, attributes, legacyTargetOverrideFlag) {
+  const event = createAnEvent(e, eventInterface, attributes);
+
+  // tryImplForWrapper() is currently required due to use in Window.js and xmlhttprequest.js
+  return tryImplForWrapper(target)._dispatch(event, legacyTargetOverrideFlag);
+}
+
+module.exports = {
+  createAnEvent,
+  fireAnEvent
+};

lib/jsdom/living/helpers/runtime-script-errors.js
@@ -2,6 +2,7 @@
 const util = require("util");
 const idlUtils = require("../generated/utils");
 const ErrorEvent = require("../generated/ErrorEvent");
+const { createAnEvent } = require("../helpers/events");
 
 const errorReportingMode = Symbol("error reporting mode");
 
@@ -17,22 +18,17 @@ function reportAnError(line, col, target, errorObject, message, location) {
 
   target[errorReportingMode] = true;
 
-  // TODO Events: use constructor directly, once they are no longer tied to a window.
-  const event = ErrorEvent.createImpl([
-    "error",
-    {
-      bubbles: false,
-      cancelable: true,
-      message,
-      filename: location,
-      lineno: line,
-      colno: col,
-      error: errorObject
-    }
-  ]);
+  const event = createAnEvent("error", ErrorEvent, {
+    cancelable: true,
+    message,
+    filename: location,
+    lineno: line,
+    colno: col,
+    error: errorObject
+  });
 
   try {
-    target.dispatchEvent(event);
+    target._dispatch(event);
   } finally {
     target[errorReportingMode] = false;
     return event.defaultPrevented;

commit 31d14cdd584fc4b9c8f9d0aeaa7faac0cdd4754d

Fix element.blur() on a focused element
This regressed in d33cf37a4b289b14c7535487cd8be00068a3e86f as it changed the location where the event's view property was set.

Fixes #2499.

lib/jsdom/living/helpers/focusing.js
@@ -4,7 +4,7 @@ const FocusEvent = require("../generated/FocusEvent.js");
 const idlUtils = require("../generated/utils.js");
 const { isDisabled } = require("./form-controls.js");
 const { firstChildWithLocalName } = require("./traversal");
-const { fireAnEvent } = require("./events");
+const { createAnEvent } = require("./events");
 const { HTML_NS } = require("./namespaces");
 
 const focusableFormElements = new Set(["input", "select", "textarea", "button"]);
@@ -61,14 +61,16 @@ exports.fireFocusEventWithTargetAdjustment = (name, target, relatedTarget) => {
     return;
   }
 
-  if (target._defaultView) {
-    target = idlUtils.implForWrapper(target._defaultView);
-  }
-
-  fireAnEvent(name, target, FocusEvent, {
+  const event = createAnEvent(name, FocusEvent, {
     composed: true,
     relatedTarget,
     view: target._ownerDocument._defaultView,
     detail: 0
   });
+
+  if (target._defaultView) {
+    target = idlUtils.implForWrapper(target._defaultView);
+  }
+
+  target._dispatch(event);
 };

test/web-platform-tests/to-upstream/html/editing/focus/focus-management/focus-blur.html
@@ -1,0 +1,23 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>element.focus() / element.blur()</title>
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+
+<input>
+
+<script>
+"use strict";
+
+const input = document.querySelector("input");
+
+test(() => {
+  assert_false(input.matches(":focus"));
+
+  input.focus();
+  assert_true(input.matches(":focus"));
+
+  input.blur();
+  assert_false(input.matches(":focus"));
+}, "The focus() and blur() methods change the focus as expected");
+</script>