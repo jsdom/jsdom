1ebb1fcd3823882e2cee36afcd1b78217b629ee4
Domenic Denicola <d@domenic.me>
2020-01-29 12:14:28 -0500

    Re-do animation frame and timer callbacks
    This setup follows the spec more, is a lot simpler to follow, fixes #2800, and fixes #2617.

--- test/api/from-outside.js
+++ test/api/from-outside.js
@@ -2,15 +2,41 @@
 const { assert } = require("chai");
 const { describe, it } = require("mocha-sugar-free");
 const { JSDOM } = require("../..");
+const { delay } = require("../util");
 
 describe("Test cases only possible to test from the outside", () => {
-  it("should not register timer after window.close() called", () => {
+  it("window.close() should prevent timers from registering and cause them to return 0", async () => {
     const { window } = new JSDOM();
 
     assert.notEqual(window.setTimeout(() => {}, 100), undefined);
 
     window.close();
 
-    assert.equal(window.setTimeout(() => {}), undefined);
+    let ran = false;
+    assert.equal(window.setTimeout(() => {
+      ran = true;
+    }), 0);
+
+    await delay(10);
+
+    assert.equal(ran, false);
+  });
+
+  it("window.close() should stop a setInterval()", async () => {
+    const { window } = new JSDOM(`<script>
+      window.counter = 0;
+      setInterval(() => window.counter++, 10);
+    </script>`, { runScripts: "dangerously" });
+
+    await delay(55);
+    window.close();
+
+    // We can't assert it's equal to 5, because the event loop might have been busy and not fully executed all 5.
+    assert.isAtLeast(window.counter, 1);
+    const counterBeforeSecondDelay = window.counter;
+
+    await delay(50);
+
+    assert.equal(window.counter, counterBeforeSecondDelay);
   });
 });
