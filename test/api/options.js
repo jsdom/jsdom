6a7d40f7b349f6f860b339a1fdac59e96f27ece1
Gabriel Lomba <glombaaguiarcosta@gmail.com>
2020-03-27 14:50:19 +0300

    lint

--- test/api/options.js
+++ test/api/options.js
@@ -196,26 +196,26 @@ describe("API: constructor options", () => {
     it("should execute with frame windows", () => {
       class MockResourceLoader extends ResourceLoader {
         fetch() {
-          return Promise.resolve('<html><p>Inside frame</p></script></html>');
+          return Promise.resolve("<html><p>Inside frame</p></script></html>");
         }
       }
 
-      let calls = [];
+      const calls = [];
 
       const dom = new JSDOM(`<iframe src="https://example.com/page.html"></iframe>`, {
         beforeParse(window, main) {
-          calls.push({window, main});
+          calls.push({ window, main });
         },
-        resources: new MockResourceLoader(),
+        resources: new MockResourceLoader()
       });
 
       assert.strictEqual(calls.length, 2);
 
-      let mainWindowCall = calls.shift();
+      const mainWindowCall = calls.shift();
       assert.strictEqual(mainWindowCall.main, true);
       assert.strictEqual(mainWindowCall.window._globalProxy, dom.window);
 
-      let frameWindowCall = calls.shift();
+      const frameWindowCall = calls.shift();
       assert.strictEqual(frameWindowCall.main, false);
       assert.notStrictEqual(frameWindowCall.window._globalProxy, dom.window);
     });
