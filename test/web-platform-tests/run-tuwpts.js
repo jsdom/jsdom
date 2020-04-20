f3e344f37a37490966159561c56324ef56d0a485
zhangjufeng <zjffun@gmail.com>
2020-04-20 18:51:16 +0800

    WIP

--- test/web-platform-tests/run-tuwpts.js
+++ test/web-platform-tests/run-tuwpts.js
@@ -1,9 +1,31 @@
 "use strict";
 const path = require("path");
-const { describe, before, after } = require("mocha-sugar-free");
+const fs = require("fs");
+const jsYAML = require("js-yaml");
+const { Minimatch } = require("minimatch");
+const { describe, specify, before, after } = require("mocha-sugar-free");
 const { spawnSync } = require("child_process");
 const { readManifest, getPossibleTestFilePaths } = require("./wpt-manifest-utils.js");
 const startWPTServer = require("./start-wpt-server.js");
+const { Canvas } = require("../../lib/jsdom/utils.js");
+
+const validReasons = new Set([
+  "fail",
+  "fail-slow",
+  "fail-with-canvas",
+  "timeout",
+  "flaky",
+  "needs-node10",
+  "needs-node11",
+  "needs-node12",
+  "needs-canvas"
+]);
+
+const nodeMajor = Number(process.versions.node.split(".")[0]);
+const hasNode10 = nodeMajor >= 10;
+const hasNode11 = nodeMajor >= 11;
+const hasNode12 = nodeMajor >= 12;
+const hasCanvas = Boolean(Canvas);
 
 const wptPath = path.resolve(__dirname, "tests");
 const testsPath = path.resolve(__dirname, "to-upstream");
@@ -18,6 +40,14 @@ spawnSync("python", args, { cwd: wptPath, stdio: "inherit" });
 const manifest = readManifest(manifestFilename);
 const possibleTestFilePaths = getPossibleTestFilePaths(manifest);
 
+const toRunFilename = path.resolve(__dirname, "tuwpt-to-run.yaml");
+const toRunString = fs.readFileSync(toRunFilename, { encoding: "utf-8" });
+const toRunDocs = jsYAML.safeLoadAll(toRunString, { filename: toRunFilename });
+
+const minimatchers = new Map();
+
+checkToRun();
+
 let wptServerURL;
 let serverProcess;
 const runSingleWPT = require("./run-single-wpt.js")(() => wptServerURL);
@@ -31,8 +61,87 @@ after(() => {
   serverProcess.kill();
 });
 
-describe("Local tests in web-platform-test format (to-upstream)", () => {
-  for (const test of possibleTestFilePaths) {
-    runSingleWPT(test);
+describe("web-platform-tests", () => {
+  for (const toRunDoc of toRunDocs) {
+    describe(toRunDoc.DIR, () => {
+      for (const testFilePath of possibleTestFilePaths) {
+        if (testFilePath.startsWith(toRunDoc.DIR + "/")) {
+          const matchingPattern = expectationsInDoc(toRunDoc).find(pattern => {
+            const matcher = minimatchers.get(toRunDoc.DIR + "/" + pattern);
+            return matcher.match(testFilePath);
+          });
+
+          const testFile = testFilePath.slice((toRunDoc.DIR + "/").length);
+          const reason = matchingPattern && toRunDoc[matchingPattern][0];
+          const shouldSkip = ["fail-slow", "timeout", "flaky"].includes(reason) ||
+                             (["fail-with-canvas", "needs-canvas"].includes(reason) && !hasCanvas);
+          const expectFail = (reason === "fail") ||
+                             (reason === "fail-with-canvas" && hasCanvas) ||
+                             (reason === "needs-node10" && !hasNode10) ||
+                             (reason === "needs-node11" && !hasNode11) ||
+                             (reason === "needs-node12" && !hasNode12);
+
+          if (matchingPattern && shouldSkip) {
+            specify.skip(`[${reason}] ${testFile}`);
+          } else if (expectFail) {
+            const failReason = reason !== "fail" ? `: ${reason}` : "";
+            runSingleWPT(testFilePath, `[expected fail${failReason}] ${testFile}`, expectFail);
+          } else {
+            runSingleWPT(testFilePath, testFile, expectFail);
+          }
+        }
+      }
+    });
   }
 });
+
+function checkToRun() {
+  let lastDir = "";
+  for (const doc of toRunDocs) {
+    if (doc.DIR.startsWith("/")) {
+      throw new Error(`DIR entries must not start with a slash: saw "${doc.DIR}"`);
+    }
+    if (doc.DIR.endsWith("/")) {
+      throw new Error(`DIR entries must not end with a slash: saw "${doc.DIR}"`);
+    }
+
+    if (!fs.existsSync(path.resolve(__dirname, "to-upstream", doc.DIR))) {
+      throw new Error(`The directory "${doc.DIR}" does not exist`);
+    }
+
+    if (doc.DIR < lastDir) {
+      throw new Error(`Bad lexicographical directory sorting in tuwpt-to-run.yaml: ${doc.DIR} should come before ${lastDir}`);
+    }
+    lastDir = doc.DIR;
+
+    let lastPattern = "";
+    for (const pattern of expectationsInDoc(doc)) {
+      if (pattern.startsWith("/")) {
+        throw new Error(`Expectation patterns must not start with a slash: saw "${pattern}"`);
+      }
+
+      if (pattern < lastPattern) {
+        throw new Error("Bad lexicographical expectation pattern sorting in to-run.yaml: " + pattern +
+                        " should come before " + lastPattern);
+      }
+      lastPattern = pattern;
+
+      const reason = doc[pattern][0];
+      if (!validReasons.has(reason)) {
+        throw new Error(`Bad reason "${reason}" for expectation ${pattern}`);
+      }
+
+      const matcher = new Minimatch(doc.DIR + "/" + pattern);
+      if (!possibleTestFilePaths.some(filename => matcher.match(filename))) {
+        throw new Error(`Expectation pattern "${pattern}" does not match any test files`);
+      }
+      minimatchers.set(doc.DIR + "/" + pattern, matcher);
+    }
+  }
+}
+
+function expectationsInDoc(doc) {
+  const keys = Object.keys(doc);
+  keys.shift(); // get rid of the DIR key
+  return keys;
+}
