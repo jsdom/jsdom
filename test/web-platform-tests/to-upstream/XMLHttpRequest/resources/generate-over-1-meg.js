adf051356cd657bc43d8a45ee8d72e0ff216c722
Domenic Denicola <d@domenic.me>
2021-03-23 16:17:11 -0400

    Roll web platform tests
    The to-upstream XHR tests were upstreamed in https://github.com/web-platform-tests/wpt/pull/27922.

--- test/web-platform-tests/to-upstream/XMLHttpRequest/resources/generate-over-1-meg.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* eslint-env node */
-const fs = require("fs");
-const path = require("path");
-// Node.js script. Run it using `node generate-over-1-meg.js`
-
-const string = "abcd".repeat(290000);
-fs.writeFileSync(path.resolve(__dirname, "over-1-meg.txt"), string);
