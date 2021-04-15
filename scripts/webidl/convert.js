84f80ce50f722b590b824b08a61277ffd208b4aa
ninevra <ninevra@gmail.com>
2021-04-15 15:32:49 -0700

    Register ReadableStream interfaces

--- scripts/webidl/convert.js
+++ scripts/webidl/convert.js
@@ -158,6 +158,7 @@ addDir("../../lib/jsdom/living/navigator");
 addDir("../../lib/jsdom/living/nodes");
 addDir("../../lib/jsdom/living/range");
 addDir("../../lib/jsdom/living/selection");
+addDir("../../lib/jsdom/living/streams");
 addDir("../../lib/jsdom/living/svg");
 addDir("../../lib/jsdom/living/traversal");
 addDir("../../lib/jsdom/living/websockets");
