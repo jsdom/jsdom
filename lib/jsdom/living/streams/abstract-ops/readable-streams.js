jsdom/jsdom

commit beaac8fc9e2a4b6070e5901e61fda58f4e0c4a0f

Remove the promise sidetable
Should help clarify when a value is a promise _capability_ versus just a 
promise.

lib/jsdom/living/streams/abstract-ops/readable-streams.js
@@ -1,9 +1,7 @@
 "use strict";
 const assert = require("assert");
 
-const { promiseResolvedWith, promiseRejectedWith, newPromise, resolvePromise, rejectPromise,
-  setPromiseIsHandledToTrue } =
-  require("../helpers/webidl.js");
+const { PromiseCapability, setPromiseIsHandledToTrue } = require("../helpers/webidl.js");
 const { canTransferArrayBuffer, copyDataBlockBytes, createArrayFromList, isDetachedBuffer, transferArrayBuffer } =
   require("./ecmascript.js");
 const { isNonNegativeNumber } = require("./miscellaneous.js");
@@ -142,7 +140,7 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
   let shuttingDown = false;
 
   // This is used to keep track of the spec's requirement that we wait for ongoing writes during shutdown.
-  let currentWrite = promiseResolvedWith(undefined);
+  let currentWrite = Promise.resolve();
 
   return new Promise((resolve, reject) => {
     let abortAlgorithm;
@@ -155,7 +153,7 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
             if (dest._state === "writable") {
               return writableStreamAbort(dest, error);
             }
-            return promiseResolvedWith(undefined);
+            return Promise.resolve();
           });
         }
         if (preventCancel === false) {
@@ -163,7 +161,7 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
             if (source._state === "readable") {
               return readableStreamCancel(source, error);
             }
-            return promiseResolvedWith(undefined);
+            return Promise.resolve();
           });
         }
         shutdownWithAction(() => Promise.all(actions.map(action => action()), results => results), true, error);
@@ -196,10 +194,10 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
 
     function pipeStep() {
       if (shuttingDown === true) {
-        return promiseResolvedWith(true);
+        return Promise.resolve(true);
       }
 
-      return writer._readyPromise.then(() => {
+      return writer._readyPromiseCap.promise.then(() => {
         return new Promise((resolveRead, rejectRead) => {
           readableStreamDefaultReaderRead(
             reader,
@@ -219,7 +217,7 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
     }
 
     // Errors must be propagated forward
-    isOrBecomesErrored(source, reader._closedPromise, storedError => {
+    isOrBecomesErrored(source, reader._closedPromiseCap.promise, storedError => {
       if (preventAbort === false) {
         shutdownWithAction(() => writableStreamAbort(dest, storedError), true, storedError);
       } else {
@@ -228,7 +226,7 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
     });
 
     // Errors must be propagated backward
-    isOrBecomesErrored(dest, writer._closedPromise, storedError => {
+    isOrBecomesErrored(dest, writer._closedPromiseCap.promise, storedError => {
       if (preventCancel === false) {
         shutdownWithAction(() => readableStreamCancel(source, storedError), true, storedError);
       } else {
@@ -237,7 +235,7 @@ function readableStreamPipeTo(globalObject, source, dest, preventClose, preventA
     });
 
     // Closing must be propagated forward
-    isOrBecomesClosed(source, reader._closedPromise, () => {
+    isOrBecomesClosed(source, reader._closedPromiseCap.promise, () => {
       if (preventClose === false) {
         shutdownWithAction(() => writableStreamDefaultWriterCloseWithErrorPropagation(writer));
       } else {
@@ -346,20 +344,22 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
     branch1,
     branch2;
 
-  const cancelPromise = newPromise();
+  const cancelPromiseCap = new PromiseCapability();
 
   function pullAlgorithm() {
     if (reading === true) {
-      return promiseResolvedWith(undefined);
+      return PromiseCapability.resolved();
     }
 
     reading = true;
 
     const readRequest = {
       chunkSteps: value => {
-        // This needs to be delayed a microtask because it takes at least a microtask to detect errors (using
-        // reader._closedPromise below), and we want errors in stream to error both branches immediately. We cannot let
-        // successful synchronously-available reads get ahead of asynchronously-available errors.
+        // This needs to be delayed a microtask because it takes at least a
+        // microtask to detect errors (using reader._closedPromiseCap below),
+        // and we want errors in stream to error both branches immediately. We
+        // cannot let successful synchronously-available reads get ahead of
+        // asynchronously-available errors.
         queueMicrotask(() => {
           reading = false;
           const value1 = value;
@@ -389,7 +389,7 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
           readableStreamDefaultControllerClose(branch2._controller);
         }
         if (canceled1 === false || canceled2 === false) {
-          resolvePromise(cancelPromise, undefined);
+          cancelPromiseCap.resolve();
         }
       },
       errorSteps: () => {
@@ -398,7 +398,7 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
     };
     readableStreamDefaultReaderRead(reader, readRequest);
 
-    return promiseResolvedWith(undefined);
+    return Promise.resolve();
   }
 
   function cancel1Algorithm(reason) {
@@ -407,9 +407,9 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
     if (canceled2 === true) {
       const compositeReason = createArrayFromList([reason1, reason2]);
       const cancelResult = readableStreamCancel(stream, compositeReason);
-      resolvePromise(cancelPromise, cancelResult);
+      cancelPromiseCap.resolve(cancelResult);
     }
-    return cancelPromise;
+    return cancelPromiseCap.promise;
   }
 
   function cancel2Algorithm(reason) {
@@ -418,9 +418,9 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
     if (canceled1 === true) {
       const compositeReason = createArrayFromList([reason1, reason2]);
       const cancelResult = readableStreamCancel(stream, compositeReason);
-      resolvePromise(cancelPromise, cancelResult);
+      cancelPromiseCap.resolve(cancelResult);
     }
-    return cancelPromise;
+    return cancelPromiseCap.promise;
   }
 
   function startAlgorithm() {}
@@ -428,11 +428,11 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
   branch1 = createReadableStream(globalObject, startAlgorithm, pullAlgorithm, cancel1Algorithm);
   branch2 = createReadableStream(globalObject, startAlgorithm, pullAlgorithm, cancel2Algorithm);
 
-  reader._closedPromise.catch(r => {
+  reader._closedPromiseCap.promise.catch(r => {
     readableStreamDefaultControllerError(branch1._controller, r);
     readableStreamDefaultControllerError(branch2._controller, r);
     if (canceled1 === false || canceled2 === false) {
-      resolvePromise(cancelPromise, undefined);
+      cancelPromiseCap.resolve();
     }
   });
 
@@ -459,10 +459,10 @@ function readableStreamCancel(stream, reason) {
   stream._disturbed = true;
 
   if (stream._state === "closed") {
-    return promiseResolvedWith(undefined);
+    return Promise.resolve();
   }
   if (stream._state === "errored") {
-    return promiseRejectedWith(stream._storedError);
+    return Promise.reject(stream._storedError);
   }
 
   readableStreamClose(stream);
@@ -490,7 +490,7 @@ function readableStreamClose(stream) {
     return;
   }
 
-  resolvePromise(reader._closedPromise, undefined);
+  reader._closedPromiseCap.resolve();
 
   if (ReadableStreamDefaultReader.isImpl(reader)) {
     for (const readRequest of reader._readRequests) {
@@ -512,8 +512,8 @@ function readableStreamError(stream, e) {
     return;
   }
 
-  rejectPromise(reader._closedPromise, e);
-  setPromiseIsHandledToTrue(reader._closedPromise);
+  reader._closedPromiseCap.reject(e);
+  setPromiseIsHandledToTrue(reader._closedPromiseCap.promise);
 
   if (ReadableStreamDefaultReader.isImpl(reader)) {
     for (const readRequest of reader._readRequests) {
@@ -607,14 +607,14 @@ function readableStreamReaderGenericInitialize(reader, stream) {
   stream._reader = reader;
 
   if (stream._state === "readable") {
-    reader._closedPromise = newPromise();
+    reader._closedPromiseCap = new PromiseCapability();
   } else if (stream._state === "closed") {
-    reader._closedPromise = promiseResolvedWith(undefined);
+    reader._closedPromiseCap = PromiseCapability.resolved();
   } else {
     assert(stream._state === "errored");
 
-    reader._closedPromise = promiseRejectedWith(stream._storedError);
-    setPromiseIsHandledToTrue(reader._closedPromise);
+    reader._closedPromiseCap = PromiseCapability.rejected(stream._storedError);
+    setPromiseIsHandledToTrue(reader._closedPromiseCap.promise);
   }
 }
 
@@ -623,16 +623,15 @@ function readableStreamReaderGenericRelease(reader) {
   assert(reader._stream._reader === reader);
 
   if (reader._stream._state === "readable") {
-    rejectPromise(
-      reader._closedPromise,
+    reader._closedPromiseCap.reject(
       new TypeError("Reader was released and can no longer be used to monitor the stream's closedness")
     );
   } else {
-    reader._closedPromise = promiseRejectedWith(
+    reader._closedPromiseCap = PromiseCapability.rejected(
       new TypeError("Reader was released and can no longer be used to monitor the stream's closedness")
     );
   }
-  setPromiseIsHandledToTrue(reader._closedPromise);
+  setPromiseIsHandledToTrue(reader._closedPromiseCap.promise);
 
   reader._stream._reader = undefined;
   reader._stream = undefined;
@@ -876,7 +875,7 @@ function setUpReadableStreamDefaultController(
   stream._controller = controller;
 
   const startResult = startAlgorithm();
-  promiseResolvedWith(startResult).then(
+  Promise.resolve(startResult).then(
     () => {
       controller._started = true;
 
@@ -903,8 +902,8 @@ function setUpReadableStreamDefaultControllerFromUnderlyingSource(
 
   /* eslint-disable func-style */
   let startAlgorithm = () => undefined;
-  let pullAlgorithm = () => promiseResolvedWith(undefined);
-  let cancelAlgorithm = () => promiseResolvedWith(undefined);
+  let pullAlgorithm = () => Promise.resolve();
+  let cancelAlgorithm = () => Promise.resolve();
   /* eslint-enable func-style */
 
   if ("start" in underlyingSourceDict) {
@@ -1474,7 +1473,7 @@ function setUpReadableByteStreamController(
   stream._controller = controller;
 
   const startResult = startAlgorithm();
-  promiseResolvedWith(startResult).then(
+  Promise.resolve(startResult).then(
     () => {
       controller._started = true;
 
@@ -1500,8 +1499,8 @@ function setUpReadableByteStreamControllerFromUnderlyingSource(
 
   /* eslint-disable func-style */
   let startAlgorithm = () => undefined;
-  let pullAlgorithm = () => promiseResolvedWith(undefined);
-  let cancelAlgorithm = () => promiseResolvedWith(undefined);
+  let pullAlgorithm = () => Promise.resolve();
+  let cancelAlgorithm = () => Promise.resolve();
   /* eslint-enable func-style */
 
   if ("start" in underlyingSourceDict) {

lib/jsdom/living/streams/ReadableStream-impl.js
@@ -1,8 +1,7 @@
 "use strict";
 const assert = require("assert");
 
-const { newPromise, resolvePromise, rejectPromise, promiseResolvedWith, promiseRejectedWith,
-  setPromiseIsHandledToTrue } = require("./helpers/webidl.js");
+const { PromiseCapability, setPromiseIsHandledToTrue } = require("./helpers/webidl.js");
 const { extractHighWaterMark, extractSizeAlgorithm } = require("./abstract-ops/queuing-strategy.js");
 const aos = require("./abstract-ops/readable-streams.js");
 const wsAOs = require("./abstract-ops/writable-streams.js");
@@ -56,7 +55,7 @@ exports.implementation = class ReadableStreamImpl {
 
   cancel(reason) {
     if (aos.isReadableStreamLocked(this) === true) {
-      return promiseRejectedWith(new TypeError("Cannot cancel a stream that already has a reader"));
+      return Promise.reject(new TypeError("Cannot cancel a stream that already has a reader"));
     }
 
     return aos.readableStreamCancel(this, reason);
@@ -102,12 +101,12 @@ exports.implementation = class ReadableStreamImpl {
 
   pipeTo(destination, options) {
     if (aos.isReadableStreamLocked(this) === true) {
-      return promiseRejectedWith(
+      return Promise.reject(
         new TypeError("ReadableStream.prototype.pipeTo cannot be used on a locked ReadableStream")
       );
     }
     if (wsAOs.isWritableStreamLocked(destination) === true) {
-      return promiseRejectedWith(
+      return Promise.reject(
         new TypeError("ReadableStream.prototype.pipeTo cannot be used on a locked WritableStream")
       );
     }
@@ -136,31 +135,31 @@ exports.implementation = class ReadableStreamImpl {
   [idlUtils.asyncIteratorNext](iterator) {
     const reader = iterator._reader;
     if (reader._stream === undefined) {
-      return promiseRejectedWith(
+      return Promise.reject(
         new TypeError("Cannot get the next iteration result once the reader has been released")
       );
     }
 
-    const promise = newPromise();
+    const promiseCap = new PromiseCapability();
     const readRequest = {
-      chunkSteps: chunk => resolvePromise(promise, chunk),
+      chunkSteps: chunk => promiseCap.resolve(chunk),
       closeSteps: () => {
         aos.readableStreamReaderGenericRelease(reader);
-        resolvePromise(promise, idlUtils.asyncIteratorEOI);
+        promiseCap.resolve(idlUtils.asyncIteratorEOI);
       },
       errorSteps: e => {
         aos.readableStreamReaderGenericRelease(reader);
-        rejectPromise(promise, e);
+        promiseCap.reject(e);
       }
     };
     aos.readableStreamDefaultReaderRead(reader, readRequest);
-    return promise;
+    return promiseCap.promise;
   }
 
   [idlUtils.asyncIteratorReturn](iterator, arg) {
     const reader = iterator._reader;
     if (reader._stream === undefined) {
-      return promiseResolvedWith(undefined);
+      return Promise.resolve();
     }
 
     assert(reader._readRequests.length === 0);
@@ -172,6 +171,6 @@ exports.implementation = class ReadableStreamImpl {
     }
 
     aos.readableStreamReaderGenericRelease(reader);
-    return promiseResolvedWith(undefined);
+    return Promise.resolve();
   }
 };

lib/jsdom/living/streams/ReadableStreamBYOBReader-impl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { newPromise, resolvePromise, rejectPromise, promiseRejectedWith } = require("./helpers/webidl.js");
+const { PromiseCapability } = require("./helpers/webidl.js");
 const { isDetachedBuffer } = require("./abstract-ops/ecmascript.js");
 const aos = require("./abstract-ops/readable-streams.js");
 const { mixin } = require("../../utils");
@@ -13,27 +13,27 @@ class ReadableStreamBYOBReaderImpl {
 
   read(view) {
     if (view.byteLength === 0) {
-      return promiseRejectedWith(new TypeError("view must have non-zero byteLength"));
+      return Promise.reject(new TypeError("view must have non-zero byteLength"));
     }
     if (view.buffer.byteLength === 0) {
-      return promiseRejectedWith(new TypeError("view's buffer must have non-zero byteLength"));
+      return Promise.reject(new TypeError("view's buffer must have non-zero byteLength"));
     }
     if (isDetachedBuffer(view.buffer) === true) {
-      return promiseRejectedWith(new TypeError("view's buffer has been detached"));
+      return Promise.reject(new TypeError("view's buffer has been detached"));
     }
 
     if (this._stream === undefined) {
-      return promiseRejectedWith(readerLockException("read"));
+      return Promise.reject(readerLockException("read"));
     }
 
-    const promise = newPromise();
+    const promiseCap = new PromiseCapability();
     const readIntoRequest = {
-      chunkSteps: chunk => resolvePromise(promise, { value: chunk, done: false }),
-      closeSteps: chunk => resolvePromise(promise, { value: chunk, done: true }),
-      errorSteps: e => rejectPromise(promise, e)
+      chunkSteps: chunk => promiseCap.resolve({ value: chunk, done: false }),
+      closeSteps: chunk => promiseCap.resolve({ value: chunk, done: true }),
+      errorSteps: e => promiseCap.reject(e)
     };
     aos.readableStreamBYOBReaderRead(this, view, readIntoRequest);
-    return promise;
+    return promiseCap.promise;
   }
 
   releaseLock() {

lib/jsdom/living/streams/ReadableStreamDefaultReader-impl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { newPromise, resolvePromise, rejectPromise, promiseRejectedWith } = require("./helpers/webidl.js");
+const { PromiseCapability } = require("./helpers/webidl.js");
 const aos = require("./abstract-ops/readable-streams.js");
 const { mixin } = require("../../utils");
 const ReadableStreamGenericReaderImpl = require("./ReadableStreamGenericReader-impl.js").implementation;
@@ -12,18 +12,18 @@ class ReadableStreamDefaultReaderImpl {
 
   read() {
     if (this._stream === undefined) {
-      return promiseRejectedWith(readerLockException("read from"));
+      return Promise.reject(readerLockException("read from"));
     }
 
-    const promise = newPromise();
+    const promiseCap = new PromiseCapability();
     const readRequest = {
-      chunkSteps: chunk => resolvePromise(promise, { value: chunk, done: false }),
-      closeSteps: () => resolvePromise(promise, { value: undefined, done: true }),
-      errorSteps: e => rejectPromise(promise, e)
+      chunkSteps: chunk => promiseCap.resolve({ value: chunk, done: false }),
+      closeSteps: () => promiseCap.resolve({ value: undefined, done: true }),
+      errorSteps: e => promiseCap.reject(e)
     };
 
     aos.readableStreamDefaultReaderRead(this, readRequest);
-    return promise;
+    return promiseCap.promise;
   }
 
   releaseLock() {

lib/jsdom/living/streams/ReadableStreamGenericReader-impl.js
@@ -1,16 +1,15 @@
 "use strict";
 
-const { promiseRejectedWith } = require("./helpers/webidl.js");
 const aos = require("./abstract-ops/readable-streams.js");
 
 exports.implementation = class ReadableStreamGenericReaderImpl {
   get closed() {
-    return this._closedPromise;
+    return this._closedPromiseCap.promise;
   }
 
   cancel(reason) {
     if (this._stream === undefined) {
-      return promiseRejectedWith(readerLockException("cancel"));
+      return Promise.reject(readerLockException("cancel"));
     }
 
     return aos.readableStreamReaderGenericCancel(this, reason);

lib/jsdom/living/streams/TransformStream-impl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { newPromise, resolvePromise } = require("./helpers/webidl.js");
+const { PromiseCapability } = require("./helpers/webidl.js");
 const { extractHighWaterMark, extractSizeAlgorithm } = require("./abstract-ops/queuing-strategy.js");
 const aos = require("./abstract-ops/transform-streams.js");
 
@@ -25,12 +25,12 @@ exports.implementation = class TransformStreamImpl {
     const writableHighWaterMark = extractHighWaterMark(writableStrategy, 1);
     const writableSizeAlgorithm = extractSizeAlgorithm(writableStrategy);
 
-    const startPromise = newPromise();
+    const startPromiseCap = new PromiseCapability();
 
     aos.initializeTransformStream(
       this._globalObject,
       this,
-      startPromise,
+      startPromiseCap.promise,
       writableHighWaterMark,
       writableSizeAlgorithm,
       readableHighWaterMark,
@@ -39,9 +39,9 @@ exports.implementation = class TransformStreamImpl {
     aos.setUpTransformStreamDefaultControllerFromTransformer(this._globalObject, this, transformer, transformerDict);
 
     if ("start" in transformerDict) {
-      resolvePromise(startPromise, transformerDict.start.call(transformer, this._controller));
+      startPromiseCap.resolve(transformerDict.start.call(transformer, this._controller));
     } else {
-      resolvePromise(startPromise, undefined);
+      startPromiseCap.resolve();
     }
   }
 

lib/jsdom/living/streams/WritableStream-impl.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const { promiseRejectedWith } = require("./helpers/webidl.js");
 const { extractHighWaterMark, extractSizeAlgorithm } = require("./abstract-ops/queuing-strategy.js");
 const aos = require("./abstract-ops/writable-streams.js");
 
@@ -37,7 +36,7 @@ exports.implementation = class WritableStreamImpl {
 
   abort(reason) {
     if (aos.isWritableStreamLocked(this) === true) {
-      return promiseRejectedWith(new TypeError("Cannot abort a stream that already has a writer"));
+      return Promise.reject(new TypeError("Cannot abort a stream that already has a writer"));
     }
 
     return aos.writableStreamAbort(this, reason);
@@ -45,11 +44,11 @@ exports.implementation = class WritableStreamImpl {
 
   close() {
     if (aos.isWritableStreamLocked(this) === true) {
-      return promiseRejectedWith(new TypeError("Cannot close a stream that already has a writer"));
+      return Promise.reject(new TypeError("Cannot close a stream that already has a writer"));
     }
 
     if (aos.writableStreamCloseQueuedOrInFlight(this) === true) {
-      return promiseRejectedWith(new TypeError("Cannot close an already-closing stream"));
+      return Promise.reject(new TypeError("Cannot close an already-closing stream"));
     }
 
     return aos.writableStreamClose(this);

lib/jsdom/living/streams/WritableStreamDefaultWriter-impl.js
@@ -1,7 +1,6 @@
 "use strict";
 const assert = require("assert");
 
-const { promiseRejectedWith } = require("./helpers/webidl.js");
 const aos = require("./abstract-ops/writable-streams.js");
 
 exports.implementation = class WritableStreamDefaultWriterImpl {
@@ -10,7 +9,7 @@ exports.implementation = class WritableStreamDefaultWriterImpl {
   }
 
   get closed() {
-    return this._closedPromise;
+    return this._closedPromiseCap.promise;
   }
 
   get desiredSize() {
@@ -22,12 +21,12 @@ exports.implementation = class WritableStreamDefaultWriterImpl {
   }
 
   get ready() {
-    return this._readyPromise;
+    return this._readyPromiseCap.promise;
   }
 
   abort(reason) {
     if (this._stream === undefined) {
-      return promiseRejectedWith(defaultWriterLockException("abort"));
+      return Promise.reject(defaultWriterLockException("abort"));
     }
 
     return aos.writableStreamDefaultWriterAbort(this, reason);
@@ -37,11 +36,11 @@ exports.implementation = class WritableStreamDefaultWriterImpl {
     const stream = this._stream;
 
     if (stream === undefined) {
-      return promiseRejectedWith(defaultWriterLockException("close"));
+      return Promise.reject(defaultWriterLockException("close"));
     }
 
     if (aos.writableStreamCloseQueuedOrInFlight(stream) === true) {
-      return promiseRejectedWith(new TypeError("Cannot close an already-closing stream"));
+      return Promise.reject(new TypeError("Cannot close an already-closing stream"));
     }
 
     return aos.writableStreamDefaultWriterClose(this);
@@ -61,7 +60,7 @@ exports.implementation = class WritableStreamDefaultWriterImpl {
 
   write(chunk) {
     if (this._stream === undefined) {
-      return promiseRejectedWith(defaultWriterLockException("write to"));
+      return Promise.reject(defaultWriterLockException("write to"));
     }
 
     return aos.writableStreamDefaultWriterWrite(this, chunk);

commit 0104cb5aeb74ea2f038ae7f957d42dd0c76392d6

Fix bug in readableStreamTee
Introduced in beaac8fc9 ("Remove the promise sidetable")

lib/jsdom/living/streams/abstract-ops/readable-streams.js
@@ -344,7 +344,7 @@ function readableStreamTee(globalObject, stream, cloneForBranch2) {
 
   function pullAlgorithm() {
     if (reading === true) {
-      return PromiseCapability.resolved();
+      return Promise.resolve();
     }
 
     reading = true;