e3d4a70112119c4c37bf3bf90af91dd170da542e
GalenWong <wonggalen1999@gmail.com>
2021-07-11 16:48:26 -0400

    Add test for js-globals

---
+++ test/api/js-globals-globalThis.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+  * This test script is NOT included in index.js. It is ran from `yarn test`
+  * directly to prevent the contamination of gloablThis in the Mocha test
+  * environment. The test passes if this script executes without error.
+ */
+
+// Attempts to polyfill globalThis.
+global.globalThis = global;
+const { JSDOM } = require("../..");
+// eslint-disable-next-line no-new
+new JSDOM("", { runScripts: "dangerously" });
