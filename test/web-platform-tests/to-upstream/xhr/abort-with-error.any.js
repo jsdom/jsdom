fea788ba74d22418f98bedb4c6a52e8f9b00a8b0
Matthew Molloy <whamtet@gmail.com>
2022-10-29 18:14:57 +0900

    Fix xhr.abort() when the XHR is invalid

---
+++ test/web-platform-tests/to-upstream/xhr/abort-with-error.any.js
@@ -0,0 +1,16 @@
+"use strict";
+// META: title=XMLHttpRequest: abort() still works when error thrown internally
+
+const test_runner = async_test();
+
+test_runner.step(() => {
+  const client = new XMLHttpRequest();
+
+  client.open("GET", "invalid-protocol://example.com", true);
+  client.onabort = test_runner.step_func(() => {
+    test_runner.done();
+  });
+
+  client.send(null);
+  client.abort();
+});
