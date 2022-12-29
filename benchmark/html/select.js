dc20dbc285d8770bbdfd7b40c93b8d10c9281c9a
eps1lon <silbermann.sebastian@gmail.com>
2022-12-29 13:28:24 +0100

    Revert "Improve performance of appending <option> to <select>"
    This reverts commit 3a569a65e795394391ddcd71d566413f52105330.

--- benchmark/html/select.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const suite = require("../document-suite");
-
-exports.text = suite(document => {
-  document.body.innerHTML = `
-  <select>
-    ${"<option value=\"volvo\">Volvo</option>".repeat(5000)}
-  </select>`;
-});
