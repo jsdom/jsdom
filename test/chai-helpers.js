44a58be5b3cbfb9630ffdd1a881ed3a5dad23ed4
Domenic Denicola <d@domenic.me>
2023-01-07 19:15:18 +0900

    Fix JSDOM.fromURL() bugs in the browser build
    Extracted from #3393.
    
    Co-authored-by: LungZeno <lungzenoopen@gmail.com>

--- test/chai-helpers.js
+++ test/chai-helpers.js
@@ -49,7 +49,9 @@ module.exports = (chai, util) => {
 
   assert.isRejected = (promise, errorType) => {
     return promise.then(
-      () => "expected promise to reject, not fulfill",
+      () => {
+        assert.fail("expected promise to reject, not fulfill");
+      },
       reason => {
         if (errorType) {
           assert.strictEqual(reason.name, errorType.name);
