9e77b1882c35570fdbc8b6dd98ae73371efe8eb4
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-01-26 23:05:19 +0900

    Add benchmark
    # dom/get-computed-style/getComputedStyle #
    jsdom  x 24,796 ops/sec ±2.27% (92 runs sampled)

---
+++ benchmark/dom/get-computed-style.js
@@ -0,0 +1,16 @@
+"use strict";
+const suite = require("../document-suite");
+
+exports.getComputedStyle = () => {
+  let window, document;
+
+  return suite({
+    setup(doc) {
+      window = doc.defaultView;
+      document = doc;
+    },
+    fn() {
+      window.getComputedStyle(document.body)
+    }
+  });
+};
