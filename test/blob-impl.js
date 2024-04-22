292366bebdc1245dbaeedfd7ec9f77bff6bf7aba
ArpanSolanki29 <42803176+ArpanSolanki29@users.noreply.github.com>
2024-04-22 15:25:16 +0530

    test: blob-impl: refactor

--- test/blob-impl.js
+++ test/blob-impl.js
@@ -2,18 +2,21 @@
 const assert = require("node:assert/strict");
 const { describe, it } = require("mocha-sugar-free");
 
-const BlobImpl = require("../../lib/jsdom/living/file-api/Blob-impl.js");
+const BlobImpl =
+  require("../../lib/jsdom/living/file-api/Blob-impl.js").implementation;
 const { TextDecoder } = require("util");
 global.TextDecoder = TextDecoder;
 
 describe("File-API: Blob-impl.js", () => {
-  it("should return correct test from the blob", done => {
+  it("should return correct test from the blob", (done) => {
     const globalObject = {};
     const blobData = "Hello World";
     const blobPros = { type: "text/plain" };
-    const blob = new BlobImpl.implementation(globalObject, [[blobData], blobPros], {});
+    const blob = new BlobImpl(globalObject, [[blobData], blobPros], {});
 
-    blob.text().then(text => {
+    blob
+      .text()
+      .then((text) => {
         assert.strictEqual(text, blobData, 'Blob text should be "Hello World"');
         done();
       })
