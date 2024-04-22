2937685d6151b58c5a8e9116a8f3bc52102e6b1f
ArpanSolanki29 <42803176+ArpanSolanki29@users.noreply.github.com>
2024-04-22 15:01:05 +0530

    Create blob-impl.js UT

---
+++ test/blob-impl.js
@@ -0,0 +1,24 @@
+"use strict";
+const assert = require("node:assert/strict");
+const { describe, it } = require("mocha-sugar-free");
+
+const BlobImpl = require("../lib/jsdom/living/file-api/Blob-impl.js");
+const TextDecoder = require("util").TextDecoder;
+global.TextDecoder = TextDecoder;
+
+describe("File-API: Blob-impl.js", () => {
+  it("should return correct test from the blob", (done) => {
+    const globalObject = {};
+    const blobData = "Hello World";
+    const blobPros = { type: "text/plain" };
+    const blob = new BlobImpl(globalObject, [[blobData], blobPros], {});
+
+    blob
+      .text()
+      .then((text) => {
+        assert.strictEqual(text, blobData, 'Blob text should be "Hello World"');
+        done();
+      })
+      .catch(done);
+  });
+});
