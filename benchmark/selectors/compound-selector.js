a41ffd95a669a14ba84012f733e5138e68dc5c8f
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-08-15 21:16:41 +0900

    Add / update benchmarks

---
+++ benchmark/selectors/compound-selector.js
@@ -0,0 +1,194 @@
+"use strict";
+const suite = require("../document-suite");
+
+exports.matches = () => {
+  let node;
+
+  return suite({
+    setup(document) {
+      const x = 5;
+      const y = 5;
+      const z = 5;
+      const xFrag = document.createDocumentFragment();
+      for (let i = 0; i < x; i++) {
+        const xNode = document.createElement("div");
+        xNode.id = `box${i}`;
+        xNode.classList.add("box", "container");
+        const yFrag = document.createDocumentFragment();
+        for (let j = 0; j < y; j++) {
+          const yNode = document.createElement("div");
+          yNode.id = `div${i}-${j}`;
+          yNode.classList.add("block", "outer");
+          for (let k = 0; k < z; k++) {
+            const zNode = document.createElement("div");
+            zNode.id = `div${i}-${j}-${k}`;
+            zNode.classList.add("block", "inner");
+            const p = document.createElement("p");
+            p.id = `p${i}-${j}-${k}`;
+            p.classList.add("content");
+            p.textContent = `${i}-${j}-${k}`;
+            zNode.append(p);
+            yNode.append(zNode);
+          }
+          yFrag.append(yNode);
+        }
+        xNode.append(yFrag);
+        xFrag.append(xNode);
+      }
+      const container = document.createElement("div");
+      container.setAttribute("id", "container");
+      container.classList.add("container");
+      container.append(xFrag);
+      document.body.append(container);
+      node = document.getElementById(`p${x - 1}-${y - 1}-${z - 1}`);
+    },
+    fn() {
+      const selector = "p.content[id]:only-child";
+      node.matches(selector);
+    }
+  });
+};
+
+exports.closest = function () {
+  let node;
+
+  return suite({
+    setup(document) {
+      const x = 5;
+      const y = 5;
+      const z = 5;
+      const xFrag = document.createDocumentFragment();
+      for (let i = 0; i < x; i++) {
+        const xNode = document.createElement("div");
+        xNode.id = `box${i}`;
+        xNode.classList.add("box", "container");
+        const yFrag = document.createDocumentFragment();
+        for (let j = 0; j < y; j++) {
+          const yNode = document.createElement("div");
+          yNode.id = `div${i}-${j}`;
+          yNode.classList.add("block", "outer");
+          for (let k = 0; k < z; k++) {
+            const zNode = document.createElement("div");
+            zNode.id = `div${i}-${j}-${k}`;
+            zNode.classList.add("block", "inner");
+            const p = document.createElement("p");
+            p.id = `p${i}-${j}-${k}`;
+            p.classList.add("content");
+            p.textContent = `${i}-${j}-${k}`;
+            zNode.append(p);
+            yNode.append(zNode);
+          }
+          yFrag.append(yNode);
+        }
+        xNode.append(yFrag);
+        xFrag.append(xNode);
+      }
+      const container = document.createElement("div");
+      container.setAttribute("id", "container");
+      container.classList.add("container");
+      container.append(xFrag);
+      document.body.append(container);
+      node = document.getElementById(`p${x - 1}-${y - 1}-${z - 1}`);
+    },
+    fn() {
+      const selector = "*.container[id]:not(.box)";
+      node.closest(selector);
+    }
+  });
+};
+
+exports.querySelector = () => {
+  let node;
+
+  return suite({
+    setup(document) {
+      const x = 5;
+      const y = 5;
+      const z = 5;
+      const xFrag = document.createDocumentFragment();
+      for (let i = 0; i < x; i++) {
+        const xNode = document.createElement("div");
+        xNode.id = `box${i}`;
+        xNode.classList.add("box", "container");
+        const yFrag = document.createDocumentFragment();
+        for (let j = 0; j < y; j++) {
+          const yNode = document.createElement("div");
+          yNode.id = `div${i}-${j}`;
+          yNode.classList.add("block", "outer");
+          for (let k = 0; k < z; k++) {
+            const zNode = document.createElement("div");
+            zNode.id = `div${i}-${j}-${k}`;
+            zNode.classList.add("block", "inner");
+            const p = document.createElement("p");
+            p.id = `p${i}-${j}-${k}`;
+            p.classList.add("content");
+            p.textContent = `${i}-${j}-${k}`;
+            zNode.append(p);
+            yNode.append(zNode);
+          }
+          yFrag.append(yNode);
+        }
+        xNode.append(yFrag);
+        xFrag.append(xNode);
+      }
+      const container = document.createElement("div");
+      container.setAttribute("id", "container");
+      container.classList.add("container");
+      container.append(xFrag);
+      document.body.append(container);
+      node = document;
+    },
+    fn() {
+      const selector = "p.content[id]:only-child";
+      node.querySelector(selector);
+    }
+  });
+};
+
+exports.querySelectorAll = function () {
+  let node;
+
+  return suite({
+    setup(document) {
+      const x = 5;
+      const y = 5;
+      const z = 5;
+      const xFrag = document.createDocumentFragment();
+      for (let i = 0; i < x; i++) {
+        const xNode = document.createElement("div");
+        xNode.id = `box${i}`;
+        xNode.classList.add("box", "container");
+        const yFrag = document.createDocumentFragment();
+        for (let j = 0; j < y; j++) {
+          const yNode = document.createElement("div");
+          yNode.id = `div${i}-${j}`;
+          yNode.classList.add("block", "outer");
+          for (let k = 0; k < z; k++) {
+            const zNode = document.createElement("div");
+            zNode.id = `div${i}-${j}-${k}`;
+            zNode.classList.add("block", "inner");
+            const p = document.createElement("p");
+            p.id = `p${i}-${j}-${k}`;
+            p.classList.add("content");
+            p.textContent = `${i}-${j}-${k}`;
+            zNode.append(p);
+            yNode.append(zNode);
+          }
+          yFrag.append(yNode);
+        }
+        xNode.append(yFrag);
+        xFrag.append(xNode);
+      }
+      const container = document.createElement("div");
+      container.setAttribute("id", "container");
+      container.classList.add("container");
+      container.append(xFrag);
+      document.body.append(container);
+      node = document;
+    },
+    fn() {
+      const selector = "p.content[id]:only-child";
+      node.querySelectorAll(selector);
+    }
+  });
+};
--- benchmark/selectors/index.js
+++ benchmark/selectors/index.js
@@ -2,5 +2,7 @@
 
 module.exports = {
   "complex-selectors": require("./complex-selectors"),
+  "compound-selector": require("./compound-selector"),
+  "simple-selector": require("./simple-selector"),
   "sizzle": require("./sizzle")
 };
