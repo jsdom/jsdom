6a5448568b5f4740606cdc3c863456353696eed7
Domenic Denicola <d@domenic.me>
2025-03-23 15:57:05 +0900

    Run WPT lint on to-upstream WPTs
    Closes #2264.

--- test/web-platform-tests/run-tuwpts.js
+++ test/web-platform-tests/run-tuwpts.js
@@ -1,20 +1,38 @@
 "use strict";
+/* eslint-disable no-console */
 const path = require("path");
 const { describe, before, after } = require("mocha-sugar-free");
 const { spawnSync } = require("child_process");
 const { readManifest, getPossibleTestFilePaths } = require("./wpt-manifest-utils.js");
 const wptServer = require("./wpt-server.js");
-const { killSubprocess } = require("./utils.js");
+const { killSubprocess, spawnSyncFiltered } = require("./utils.js");
 
 const wptPath = path.resolve(__dirname, "tests");
 const testsPath = path.resolve(__dirname, "to-upstream");
-const manifestFilename = path.resolve(__dirname, "tuwpt-manifest.json");
+const relativeTestsPath = path.relative(wptPath, testsPath);
 
 // We can afford to re-generate the manifest each time; we have few enough files that it's cheap.
-const testsRootArg = path.relative(wptPath, testsPath);
-const pathArg = path.relative(wptPath, manifestFilename);
-const args = ["./wpt.py", "manifest", "--tests-root", testsRootArg, "--path", pathArg];
-spawnSync("python", args, { cwd: wptPath, stdio: "inherit" });
+const manifestFilename = path.resolve(__dirname, "tuwpt-manifest.json");
+const manifestResult = spawnSync(
+  "python",
+  ["./wpt.py", "manifest", "--tests-root", relativeTestsPath, "--path", path.relative(wptPath, manifestFilename)],
+  { cwd: wptPath, stdio: "inherit" }
+);
+if (manifestResult.status !== 0) {
+  console.error("Manifest generation failed");
+  process.exit(1);
+}
+
+// Similarly we run the lint each time.
+const lintResult = spawnSyncFiltered(
+  "python",
+  ["./wpt.py", "lint", "--repo-root", relativeTestsPath, "--all"],
+  { cwd: wptPath }
+);
+if (lintResult.status !== 0) {
+  console.error("Linting failed");
+  process.exit(1);
+}
 
 const manifest = readManifest(manifestFilename);
 const possibleTestFilePaths = getPossibleTestFilePaths(manifest);
