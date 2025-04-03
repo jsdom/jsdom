94c91f272694134d091372081c41fd24b41e4471
asamuzaK (Kazz) <kazz@asamuzak.jp>
2025-04-03 23:56:27 +0900

    Update benchmark tests
    Add reflow step to avoid cache

--- benchmark/selectors/sizzle.js
+++ benchmark/selectors/sizzle.js
@@ -30,6 +30,10 @@ exports.querySelectorAll = function () {
     fn() {
       for (const selector of selectors) {
         try {
+          const div = document.createElement("div");
+          document.body.appendChild(div);
+          document.querySelectorAll(selector);
+          div.remove();
           document.querySelectorAll(selector);
         } catch {
           count++;
@@ -71,6 +75,10 @@ exports.querySelector = function () {
     fn() {
       for (const selector of selectors) {
         try {
+          const div = document.createElement("div");
+          document.body.appendChild(div);
+          document.querySelector(selector);
+          div.remove();
           document.querySelector(selector);
         } catch {
           count++;
