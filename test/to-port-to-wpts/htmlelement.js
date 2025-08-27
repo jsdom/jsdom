c163782be1e7c3dcb85d784517fa5b5430d24d44
Ash Searle <ash@hexmen.com>
2025-08-27 21:04:49 +0100

    Fix #3882 add search tag and ensure all non-inherited tags are tested

--- test/to-port-to-wpts/htmlelement.js
+++ test/to-port-to-wpts/htmlelement.js
@@ -6,9 +6,9 @@ const { describe, specify } = require("mocha-sugar-free");
 const { JSDOM } = require("../..");
 
 const nonInheritedTags = [
-  "article", "section", "nav", "aside", "hgroup", "header", "footer", "address", "dt",
-  "dd", "figure", "figcaption", "main", "em", "strong", "small", "s", "cite", "abbr",
-  "code", "i", "b", "u"
+  "abbr", "address", "article", "aside", "b", "bdi", "bdo", "cite", "code", "dd", "dfn", "dt", "em", "figcaption",
+  "figure", "footer", "header", "hgroup", "i", "kbd", "main", "mark", "nav", "noscript", "rp", "rt", "ruby", "s",
+  "samp", "search", "section", "small", "strong", "sub", "summary", "sup", "u", "var", "wbr"
 ];
 
 describe("htmlelement", () => {
@@ -66,7 +66,7 @@ describe("htmlelement", () => {
     t.timeout(5000); // give this a bit of leeway. It's apparently slow
 
     for (let i = 0; i < nonInheritedTags.length; ++i) {
-      const doc = (new JSDOM("<" + nonInheritedTags[i] + ">")).window.document;
+      const doc = (new JSDOM("<body><" + nonInheritedTags[i] + "></body>")).window.document;
       const el = doc.body.firstChild;
       assert.ok(
         el.constructor === doc.defaultView.HTMLElement,
