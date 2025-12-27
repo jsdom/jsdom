d420f72d6eb2aea739f2764d7dc094aee4cdc9ad
Domenic Denicola <d@domenic.me>
2025-12-27 11:56:00 +0900

    Add style performance benchmark
    This is a port of #4001 to use our existing benchmark framework.

--- benchmark/index.js
+++ benchmark/index.js
@@ -4,7 +4,8 @@ module.exports = {
   jsdom: require("./jsdom"),
   dom: require("./dom"),
   html: require("./html"),
-  selectors: require("./selectors")
+  selectors: require("./selectors"),
+  style: require("./style")
 };
 
 require("./prepare-suites")("", module.exports);
