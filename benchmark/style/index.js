df0400d35957ca337d7146cd521ebd597f14bd57
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-01-24 08:12:02 +0900

    Update style benchmark

--- benchmark/style/index.js
+++ benchmark/style/index.js
@@ -9,7 +9,7 @@ exports["innerHTML: simple divs (no styles)"] = suite(document => {
 
 exports["innerHTML: divs with inline styles"] = suite(document => {
   document.body.innerHTML =
-    '<div style="color: blue; background-color: white; padding: 10px; border: 1px solid black;">Cell</div>'
+    '<div style="color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;">Cell</div>'
       .repeat(20);
 });
 
@@ -20,6 +20,7 @@ exports["createElement + style properties"] = suite(document => {
     div.style.backgroundColor = "white";
     div.style.padding = "10px";
     div.style.border = "1px solid black";
+    div.style.animation = "3s linear 1s slide-in";
   }
 });
 
@@ -28,7 +29,7 @@ exports["createElement + setAttribute('style')"] = suite(document => {
     const div = document.createElement("div");
     div.setAttribute(
       "style",
-      "color: blue; background-color: white; padding: 10px; border: 1px solid black;"
+      "color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;"
     );
   }
 });
@@ -37,6 +38,6 @@ exports["createElement + style.cssText"] = suite(document => {
   for (let i = 0; i < 20; i++) {
     const div = document.createElement("div");
     div.style.cssText =
-      "color: blue; background-color: white; padding: 10px; border: 1px solid black;";
+      "color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;";
   }
 });
