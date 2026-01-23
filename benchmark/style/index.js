289bf22081c7fd0465f4cea3e1c5203a36c13535
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-01-24 08:28:22 +0900

    Use constant variable

--- benchmark/style/index.js
+++ benchmark/style/index.js
@@ -3,14 +3,15 @@ const suite = require("../document-suite");
 
 // Style benchmarks based on https://github.com/jsdom/jsdom/issues/3985
 
+const cssText =
+  "color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;";
+
 exports["innerHTML: simple divs (no styles)"] = suite(document => {
   document.body.innerHTML = "<div>Hello</div>".repeat(20);
 });
 
 exports["innerHTML: divs with inline styles"] = suite(document => {
-  document.body.innerHTML =
-    '<div style="color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;">Cell</div>'
-      .repeat(20);
+  document.body.innerHTML = `<div style="${cssText}">Cell</div>`.repeat(20);
 });
 
 exports["createElement + style properties"] = suite(document => {
@@ -27,17 +28,13 @@ exports["createElement + style properties"] = suite(document => {
 exports["createElement + setAttribute('style')"] = suite(document => {
   for (let i = 0; i < 20; i++) {
     const div = document.createElement("div");
-    div.setAttribute(
-      "style",
-      "color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;"
-    );
+    div.setAttribute("style", cssText);
   }
 });
 
 exports["createElement + style.cssText"] = suite(document => {
   for (let i = 0; i < 20; i++) {
     const div = document.createElement("div");
-    div.style.cssText =
-      "color: blue; background-color: white; padding: 10px; border: 1px solid black; animation: 3s linear 1s slide-in;";
+    div.style.cssText = cssText;
   }
 });
